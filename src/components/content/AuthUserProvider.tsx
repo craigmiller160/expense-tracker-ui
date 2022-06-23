b1b859685a22b0698d46fa866cde1defde6088f3
Craig Miller <craigmiller160@gmail.com>
2022-06-23 18:54:24 -0400

    Improved auth user state

--- src/components/content/AuthUserProvider.tsx
+++ src/components/content/AuthUserProvider.tsx
@@ -4,14 +4,21 @@ import { getAuthUser } from '../../services/AuthService';
 
 export interface AuthUserContextValue {
 	readonly authUser: Resource<AuthUser | undefined>;
+	readonly hasChecked: () => boolean;
+	readonly isAuthorized: () => boolean;
 }
 
 export const AuthUserContext = createContext<AuthUserContextValue>();
 
 export const AuthUserProvider = (props: ParentProps) => {
 	const [authUser] = getAuthUser();
+	const value: AuthUserContextValue = {
+		authUser,
+		hasChecked: () => !authUser.loading || authUser.error,
+		isAuthorized: () => authUser() !== undefined
+	};
 	return (
-		<AuthUserContext.Provider value={{ authUser }}>
+		<AuthUserContext.Provider value={value}>
 			{props.children}
 		</AuthUserContext.Provider>
 	);

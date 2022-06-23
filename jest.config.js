434c66b3ec515042f81a69ac7eb26dd51562e641
Craig Miller <craigmiller160@gmail.com>
2022-06-23 13:57:56 -0400

    Added more config files

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const merge = require('@craigmiller160/config-merge');
+const jestConfig = require('@craigmiller160/jest-config');
+const jestTsConfig = require('@craigmiller160/jest-config-ts');
+const path = require('path');
+
+module.exports = merge(jestConfig, jestTsConfig, {
+	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
+});

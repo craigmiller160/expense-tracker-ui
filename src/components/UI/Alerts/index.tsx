a062726f17b16b813cd383f31f2c003906b064c0
Craig Miller <craigmiller160@gmail.com>
2022-06-26 14:51:39 -0400

    Added temp alert data

--- src/components/UI/Alerts/index.tsx
+++ src/components/UI/Alerts/index.tsx
@@ -1,9 +1,17 @@
 import { For } from 'solid-js';
 import Alert from '@suid/material/Alert';
 import Stack from '@suid/material/Stack';
-import { alertStore } from '../../../stores/AlertStore';
+import { alertStore, updateAlertStore } from '../../../stores/AlertStore';
 
 export const Alerts = () => {
+	// TODO delete this
+	updateAlertStore((prev) => ({
+		...prev,
+		alerts: [
+			{ type: 'error', message: 'One' },
+			{ type: 'success', message: 'Two' }
+		]
+	}));
 	return (
 		<Stack sx={{ width: '100%' }}>
 			<For each={alertStore.alerts}>

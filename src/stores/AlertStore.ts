dfc734c33a47c0e8b9596e49e2776c64c5fc7518
Craig Miller <craigmiller160@gmail.com>
2022-06-26 15:03:06 -0400

    Got alerts working

--- src/stores/AlertStore.ts
+++ src/stores/AlertStore.ts
@@ -1,7 +1,11 @@
 import { createStore } from 'solid-js/store';
 import { AlertColor } from '@suid/material/Alert';
+import { nanoid } from 'nanoid';
+
+const ALERT_TIMEOUT = 6_000;
 
 export interface AlertData {
+	readonly id: string;
 	readonly type: AlertColor;
 	readonly message: string;
 }
@@ -13,3 +17,25 @@ export interface AlertStoreData {
 export const [alertStore, updateAlertStore] = createStore<AlertStoreData>({
 	alerts: []
 });
+
+export const removeAlert = (id: string): void =>
+	updateAlertStore((prev) => ({
+		...prev,
+		alerts: prev.alerts.filter((alert) => alert.id !== id)
+	}));
+
+export const addAlert = (type: AlertColor, message: string): void => {
+	const id = nanoid();
+	setTimeout(() => removeAlert(id), ALERT_TIMEOUT);
+	updateAlertStore((prev) => ({
+		...prev,
+		alerts: [
+			...prev.alerts,
+			{
+				id,
+				type,
+				message
+			}
+		]
+	}));
+};

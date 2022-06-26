82ba31acc1f3e1c0b61503ef6688b72866b1feee
Craig Miller <craigmiller160@gmail.com>
2022-06-26 12:25:42 -0400

    Refactored content page

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,18 +1,8 @@
 import Container from '@suid/material/Container';
 import { Welcome } from './Welcome';
-import { createSignal, lazy } from 'solid-js';
-import Button from '@suid/material/Button';
-import { namedLazy } from '../../utils/solidWrappers';
 
-const Temp = namedLazy(() => import('./Temp'), 'Temp');
-
-export const Content = () => {
-	const [showTemp, setShowTemp] = createSignal(false);
-	return (
-		<Container>
-			{showTemp() && <Temp />}
-			<Button onClick={() => setShowTemp(true)}>Click</Button>
-			<Welcome />
-		</Container>
-	);
-};
+export const Content = () => (
+	<Container>
+		<Welcome />
+	</Container>
+);

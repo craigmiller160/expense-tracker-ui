c8b95cec2317e5c272dd56f5ba9c8ce67ed31e24
Craig Miller <craigmiller160@gmail.com>
2022-06-26 11:55:20 -0400

    Some eslint tweaks

--- src/utils/solidWrappers.ts
+++ src/utils/solidWrappers.ts
@@ -1,8 +1,13 @@
-import { Component, JSX, lazy } from 'solid-js';
+import { Component, lazy } from 'solid-js';
 
-export const namedLazy = (importer: () => Promise<any>, name: string) =>
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const namedLazy = (
+	importer: () => Promise<Record<string, any>>,
+	name: string
+) =>
 	lazy(() =>
 		importer().then((res) => ({
 			default: res[name] as Component<any>
 		}))
 	);
+/* eslint-enable @typescript-eslint/no-explicit-any */

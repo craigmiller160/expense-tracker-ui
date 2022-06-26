ecc8453faadc6c6decb835686c95b6539a780d91
Craig Miller <craigmiller160@gmail.com>
2022-06-26 15:08:24 -0400

    Implemented default error handler

--- src/services/AjaxApi.ts
+++ src/services/AjaxApi.ts
@@ -1,10 +1,28 @@
-import { createApi } from '@craigmiller160/ajax-api';
+import { createApi, DefaultErrorHandler } from '@craigmiller160/ajax-api';
 import { AxiosResponse } from 'axios';
+import { match } from 'ts-pattern';
+import { debounce } from 'lodash-es';
+import { addAlert } from '../stores/AlertStore';
+import { authUserResource } from '../resources/AuthResources';
 
-// TODO need global error handler
+const handleUnauthorizedError = debounce(() => {
+	const [, { refetch }] = authUserResource;
+	refetch();
+	addAlert('error', 'Unauthorized');
+}, 500);
+
+const defaultErrorHandler: DefaultErrorHandler = (status, error, message) => {
+	console.error(message, error);
+	match(status)
+		.with(401, handleUnauthorizedError)
+		.otherwise(() =>
+			addAlert('error', `${message ?? ''}: ${error.message}`)
+		);
+};
 
 export const expenseTrackerApi = createApi({
-	baseURL: '/expense-tracker/api'
+	baseURL: '/expense-tracker/api',
+	defaultErrorHandler
 });
 
 export const getData = <T>(res: AxiosResponse<T>) => res.data;

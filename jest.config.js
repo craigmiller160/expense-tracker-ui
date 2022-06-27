0e5531c5f88d2b067a6a679d119c0f6655f2a77c
Craig Miller <craigmiller160@gmail.com>
2022-06-27 09:13:34 -0400

    More attempts to get tests working

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,24 @@
 const merge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
 const jestTsConfig = require('@craigmiller160/jest-config-ts');
+const {
+	libPatterns,
+	createCombinedPattern
+} = require('@craigmiller160/jest-config/utils/libsToRecompile');
 const path = require('path');
 
-module.exports = merge(jestConfig, jestTsConfig, {
-	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
+const config = merge(jestConfig, jestTsConfig, {
+	preset: 'solid-jest/preset/browser',
+	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')],
+	transform: {
+		'^.+\\.jsx?$': 'esbuild-jest'
+	}
 });
+
+module.exports = {
+	...config,
+	transformIgnorePatterns: [
+		...config.transformIgnorePatterns.slice(1),
+		createCombinedPattern([...libPatterns, 'nanoid'])
+	]
+};

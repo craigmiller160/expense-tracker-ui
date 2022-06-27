72b7630c96806d7323924edbc67e4cdf2988f608
Craig Miller <craigmiller160@gmail.com>
2022-06-26 21:16:53 -0400

    More improvements to test database

---
+++ src/types/db.ts
@@ -0,0 +1,7 @@
+export interface DbRecord {
+	readonly id: string;
+}
+
+export interface UserRecord {
+	readonly userId: number;
+}
--- test/server/Database.ts
+++ test/server/Database.ts
@@ -3,12 +3,46 @@ import { AuthUser } from '../../src/types/auth';
 import produce from 'immer';
 import { WritableDraft } from 'immer/dist/types/types-external';
 import * as Option from 'fp-ts/es6/Option';
+import { DbRecord, UserRecord } from '../../src/types/db';
+import { nanoid } from 'nanoid';
+
+const USER_ID = 1;
 
 export interface Data {
 	readonly authUser: OptionT<AuthUser>;
 }
 
 export type DataUpdater = (draft: WritableDraft<Data>) => void;
+
+export const ensureDbRecord = <T extends object>(record: T): T & DbRecord => {
+	if (
+		Object.prototype.hasOwnProperty.call(record, 'id') &&
+		(record as DbRecord).id
+	) {
+		return record as T & DbRecord;
+	}
+	return {
+		...record,
+		id: nanoid()
+	};
+};
+
+export const ensureDbUserRecord = <T extends object>(
+	record: T
+): T & DbRecord & UserRecord => {
+	const dbRecord = ensureDbRecord(record);
+	type DbRecordType = typeof dbRecord;
+	if (
+		Object.prototype.hasOwnProperty.call(dbRecord, 'userId') &&
+		(dbRecord as DbRecordType & UserRecord).userId
+	) {
+		return dbRecord as DbRecordType & UserRecord;
+	}
+	return {
+		...dbRecord,
+		userId: USER_ID
+	};
+};
 
 export class Database {
 	data: Data = {

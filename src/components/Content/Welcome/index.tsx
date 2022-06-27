cf327e630f7550ebf0fa25cd975bd4b43e0a27fd
Craig Miller <craigmiller160@gmail.com>
2022-06-27 11:51:07 -0400

    Improved welcome page styles

--- src/components/Content/Welcome/index.tsx
+++ src/components/Content/Welcome/index.tsx
@@ -1,5 +1,6 @@
 import './Welcome.scss';
 import Typography from '@suid/material/Typography';
+
 export const Welcome = () => (
 	<div class="Welcome">
 		<Typography variant="h4">Welcome to Expense Tracker</Typography>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,14 @@
 import { RouteDefinition } from 'solid-app-router';
 import { namedLazy } from '../utils/solidWrappers';
 
+const welcomeRoute: RouteDefinition = {
+	path: '/welcome',
+	component: namedLazy(
+		() => import('../components/Content/Welcome'),
+		'Welcome'
+	)
+};
+
 export const createRoutes = (): RouteDefinition[] => {
-	return [
-		{
-			path: '/welcome',
-			component: namedLazy(
-				() => import('../components/Content/Welcome'),
-				'Welcome'
-			)
-		}
-	];
+	return [welcomeRoute];
 };

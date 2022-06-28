60d1e3a6128fcc7b692b21dd8bb3a3fe4e1e3e2e
Craig Miller <craigmiller160@gmail.com>
2022-06-28 18:21:28 -0400

    Beginning to construct UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,1 +1,8 @@
-export const App = () => <h1>Hello World</h1>;
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+
+export const App = () => (
+	<BrowserRouter basename="/expense-tracker">
+		<CssBaseline />
+	</BrowserRouter>
+);

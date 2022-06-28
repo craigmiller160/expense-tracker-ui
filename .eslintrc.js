9c7cb8055b0c31fa86f9a5f3fa70e97843086207
Craig Miller <craigmiller160@gmail.com>
2022-06-28 12:47:44 -0400

    Switched vite to react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
-    extends: [
-        '@craigmiller160/eslint-config-js',
-        '@craigmiller160/eslint-config-prettier',
-        '@craigmiller160/eslint-config-jest',
-        '@craigmiller160/eslint-config-tree-shaking-import-restrictions',
-        '@craigmiller160/eslint-config-ts',
-        'plugin:solid/typescript'
-    ],
-    plugins: [
-        'solid'
-    ]
+	extends: [
+		'@craigmiller160/eslint-config-js',
+		'@craigmiller160/eslint-config-prettier',
+		'@craigmiller160/eslint-config-jest',
+		'@craigmiller160/eslint-config-tree-shaking-import-restrictions',
+		'@craigmiller160/eslint-config-ts'
+	]
 };

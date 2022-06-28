1beb3d383509d44b5013f75f39cf1fb290ea0b7c
Craig Miller <craigmiller160@gmail.com>
2022-06-28 19:31:10 -0400

    Constructing routes

---
+++ src/utils/reactWrappers.ts
@@ -0,0 +1,13 @@
+import { ComponentType, lazy } from 'react';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const namedLazy = (
+	importer: () => Promise<Record<string, any>>,
+	name: string
+) =>
+	lazy(() =>
+		importer().then((res) => ({
+			default: res[name] as ComponentType<any>
+		}))
+	);
+/* eslint-enable @typescript-eslint/no-explicit-any */

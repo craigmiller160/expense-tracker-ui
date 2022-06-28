b3ee2da2696681d2582706ca35b011c11520ff00
Craig Miller <craigmiller160@gmail.com>
2022-06-28 18:23:36 -0400

    Integrated navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
+import { Navbar } from './Navbar';
 
 export const App = () => (
 	<BrowserRouter basename="/expense-tracker">
 		<CssBaseline />
+		<Navbar />
 	</BrowserRouter>
 );

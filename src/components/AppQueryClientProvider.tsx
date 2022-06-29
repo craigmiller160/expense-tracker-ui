d346717735e582fd0ff784228fca300e0383a7d5
Craig Miller <craigmiller160@gmail.com>
2022-06-28 22:10:26 -0400

    Incorporated error handler correctly

--- src/components/AppQueryClientProvider.tsx
+++ src/components/AppQueryClientProvider.tsx
@@ -1,15 +1,34 @@
 import { PropsWithChildren, useContext } from 'react';
-import { AlertContext } from './UI/Alerts/AlertProvider';
+import { AlertContext, AlertContextValue } from './UI/Alerts/AlertProvider';
 import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
+import { match, P } from 'ts-pattern';
+
+const concatenateMessage = (error: Error): string => {
+	let message: string = error.message;
+	let baseError = error;
+	while (baseError.cause !== undefined) {
+		baseError = baseError.cause;
+		message = `${message}; ${baseError.message}`;
+	}
+	return message;
+};
+
+const createErrorHandler =
+	(alertContext: AlertContextValue) => (error: unknown) => {
+		match(error)
+			.with(P.instanceOf(Error), (e) =>
+				alertContext.addAlert('error', concatenateMessage(e))
+			)
+			.otherwise(() =>
+				alertContext.addAlert('error', `Unknown Error: ${error}`)
+			);
+	};
 
 export const AppQueryClientProvider = (props: PropsWithChildren) => {
 	const alertContext = useContext(AlertContext);
 	const queryClient = new QueryClient({
 		queryCache: new QueryCache({
-			onError: (error) => {
-				console.log('OnError');
-				alertContext.addAlert('error', 'Have an error');
-			}
+			onError: createErrorHandler(alertContext)
 		})
 	});
 	return (

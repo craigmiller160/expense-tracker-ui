66959db829918bcd9dfce37b912b1f5a66b259a1
Craig Miller <craigmiller160@gmail.com>
2022-06-28 22:05:34 -0400

    Got the error handler to fire a single time

--- src/components/AppQueryClientProvider.tsx
+++ src/components/AppQueryClientProvider.tsx
@@ -1,18 +1,16 @@
 import { PropsWithChildren, useContext } from 'react';
 import { AlertContext } from './UI/Alerts/AlertProvider';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
 
 export const AppQueryClientProvider = (props: PropsWithChildren) => {
 	const alertContext = useContext(AlertContext);
 	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				onError: (error) => {
-					console.log('OnError');
-					alertContext.addAlert('error', 'Have an error');
-				}
+		queryCache: new QueryCache({
+			onError: (error) => {
+				console.log('OnError');
+				alertContext.addAlert('error', 'Have an error');
 			}
-		}
+		})
 	});
 	return (
 		<QueryClientProvider client={queryClient}>

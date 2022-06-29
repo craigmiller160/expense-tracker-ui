ac5847a9e49ca525a444edf6c12ad00abb7be390
Craig Miller <craigmiller160@gmail.com>
2022-06-29 09:55:27 -0400

    Cleanup

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,10 +1,8 @@
 import { useAppRoutes } from '../../routes';
 import { Alerts } from '../UI/Alerts';
-import { useGetAllCategories } from '../../ajaxapi/query/CategoryQueries';
 
 export const Content = () => {
 	const Routes = useAppRoutes();
-	useGetAllCategories(); // TODO delete this
 	return (
 		<>
 			<Alerts />

588948d249df1879e9117dcad0175fd56c0fc9f3
Craig Miller <craigmiller160@gmail.com>
2022-06-28 22:17:28 -0400

    Building out more of the error handler

--- src/components/AppQueryClientProvider.tsx
+++ src/components/AppQueryClientProvider.tsx
@@ -2,6 +2,8 @@ import { PropsWithChildren, useContext } from 'react';
 import { AlertContext, AlertContextValue } from './UI/Alerts/AlertProvider';
 import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
 import { match, P } from 'ts-pattern';
+import { NoAlertError } from '../error/NoAlertError';
+import { constVoid } from 'fp-ts/es6/function';
 
 const concatenateMessage = (error: Error): string => {
 	let message: string = error.message;
@@ -13,12 +15,18 @@ const concatenateMessage = (error: Error): string => {
 	return message;
 };
 
+// TODO need to handle 401 errors
 const createErrorHandler =
 	(alertContext: AlertContextValue) => (error: unknown) => {
 		match(error)
-			.with(P.instanceOf(Error), (e) =>
-				alertContext.addAlert('error', concatenateMessage(e))
+			.with(
+				P.intersection(
+					P.not(P.instanceOf(NoAlertError)),
+					P.instanceOf(Error)
+				),
+				(e) => alertContext.addAlert('error', concatenateMessage(e))
 			)
+			.with(P.instanceOf(NoAlertError), () => constVoid())
 			.otherwise(() =>
 				alertContext.addAlert('error', `Unknown Error: ${error}`)
 			);

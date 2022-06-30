be75ac4f9584fa1ce6f31bfea8feae3b7bc1eb23
Craig Miller <craigmiller160@gmail.com>
2022-06-30 16:00:55 -0400

    Finished service function

--- src/ajaxapi/service/TransactionImportService.ts
+++ src/ajaxapi/service/TransactionImportService.ts
@@ -1,3 +1,11 @@
-export const importTransactions = () => {
+import { FileType } from '../../types/file';
+import { expenseTrackerApi } from './AjaxApi';
 
+export const importTransactions = (type: FileType, file: File) => {
+	const form = new FormData();
+	form.append('file', file);
+	return expenseTrackerApi.post<FormData, unknown>({
+		uri: `transaction-import?type=${type}`,
+		errorCustomizer: 'Error importing transaction'
+	});
 };

4eba6be98643d87bfe60df98aa154b86510a5c5a
Craig Miller <craigmiller160@gmail.com>
2022-06-30 11:46:07 -0400

    Fixed lint issue

--- src/components/Content/Categories/CategoryDetailsDialog.tsx
+++ src/components/Content/Categories/CategoryDetailsDialog.tsx
@@ -145,7 +145,10 @@ export const CategoryDetailsDialog = (props: Props) => {
 			onClose={props.onClose}
 			actions={Actions}
 		>
-			<div className="CategoryDetailsDialog" data-testid="category-details-form">
+			<div
+				className="CategoryDetailsDialog"
+				data-testid="category-details-form"
+			>
 				<Typography variant="h6">Category Information</Typography>
 				<form onSubmit={handleSubmit(onSubmit)}>
 					<TextField

25c9585736e3ed3776163b3a92ae45827b87fcbc
Craig Miller <craigmiller160@gmail.com>
2022-06-30 15:05:33 -0400

    Committing progress

--- src/components/Content/Import/index.tsx
+++ src/components/Content/Import/index.tsx
@@ -1,13 +1,31 @@
 import './ImportTransactions.scss';
-import { Input, Typography } from '@mui/material';
+import { Button, Input, Typography } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { FileType } from '../../../types/file';
 import { useEffect } from 'react';
+import {
+	Autocomplete,
+	SelectOption
+} from '@craigmiller160/react-hook-form-material-ui';
+import { match } from 'ts-pattern';
 
 interface FormData {
 	readonly file?: File;
 	readonly fileType: FileType;
 }
+
+const getFileTypeLabel = (fileType: FileType): string =>
+	match(fileType)
+		.with(FileType.DISCOVER_CSV, () => 'Discover (CSV)')
+		.with(FileType.CHASE_CSV, () => 'Chase (CSV)')
+		.run();
+
+const FILE_TYPES = Object.keys(FileType).map(
+	(key): SelectOption<FileType> => ({
+		value: key as FileType,
+		label: getFileTypeLabel(key as FileType)
+	})
+);
 
 export const Import = () => {
 	const { control, reset, handleSubmit } = useForm<FormData>();
@@ -17,17 +35,22 @@ export const Import = () => {
 		});
 	}, []);
 
-	const onSubmit = (values: FormData) => {
-
-	};
+	const onSubmit = (values: FormData) => {};
 
 	return (
 		<div className="ImportTransactions">
 			<div className="TitleWrapper">
 				<Typography variant="h4">Import Transactions</Typography>
 			</div>
-			<form onSubmit={handleSubmit(onSubmit)}>
+			<form className="ImportForm" onSubmit={handleSubmit(onSubmit)}>
+				<Autocomplete
+					name="fileType"
+					control={control}
+					label="File Type"
+					options={FILE_TYPES}
+				/>
 				<Input type="file" />
+				<Button variant="contained">Submit</Button>
 			</form>
 		</div>
 	);

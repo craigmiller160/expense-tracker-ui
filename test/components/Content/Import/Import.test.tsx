86f9d84198986f517adf755ca215fc7597bb37a0
Craig Miller <craigmiller160@gmail.com>
2022-06-30 17:11:40 -0400

    Stubbed out more tests

--- test/components/Content/Import/Import.test.tsx
+++ test/components/Content/Import/Import.test.tsx
@@ -1,7 +1,15 @@
 export {};
 
 describe('Transaction Import', () => {
-	it('will import successfully', async () => {
+	it('imports file successfully', async () => {
+		throw new Error();
+	});
+
+	it('displays error for invalid import', async () => {
+		throw new Error();
+	});
+
+	it('prevents import of improperly filled out form', async () => {
 		throw new Error();
 	});
 });

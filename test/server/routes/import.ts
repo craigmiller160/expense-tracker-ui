89e573da5f96fad4bf2b53208d62f6f2e079253e
Craig Miller <craigmiller160@gmail.com>
2022-06-30 17:08:29 -0400

    Created test routes for import

---
+++ test/server/routes/import.ts
@@ -0,0 +1,28 @@
+import { Database } from '../Database';
+import { Server } from 'miragejs/server';
+import { Response } from 'miragejs';
+import { FileType } from '../../../src/types/file';
+import { match } from 'ts-pattern';
+import { ImportTransactionsResponse } from '../../../src/types/import';
+import { ErrorResponse } from '../../../src/types/error';
+
+const error: ErrorResponse = {
+	timestamp: '',
+	path: '',
+	message: 'Invalid CSV import',
+	method: '',
+	status: 400
+};
+
+export const createImportRoutes = (database: Database, server: Server) => {
+	server.post('/transaction-import', (schema, request) => {
+		return match(request.params.type as FileType)
+			.with(
+				FileType.CHASE_CSV,
+				(): ImportTransactionsResponse => ({
+					transactionsImported: 10
+				})
+			)
+			.otherwise(() => new Response(400, null, error));
+	});
+};

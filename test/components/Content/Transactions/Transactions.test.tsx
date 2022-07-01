410b8fe1fa4349d96efc7ff0989518937460449a
Craig Miller <craigmiller160@gmail.com>
2022-07-01 16:13:47 -0400

    More test setup tweaks

--- test/components/Content/Transactions/Transactions.test.tsx
+++ test/components/Content/Transactions/Transactions.test.tsx
@@ -1,4 +1,5 @@
 import { ApiServer, newApiServer } from '../../../server';
+import { renderApp } from '../../../testutils/renderApp';
 
 describe('Transactions', () => {
 	let apiServer: ApiServer;
@@ -11,14 +12,23 @@ describe('Transactions', () => {
 	});
 
 	it('loads and displays transactions', async () => {
+		renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
 		throw new Error();
 	});
 
 	it('can change the rows-per-page and automatically re-load the data', async () => {
+		renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
 		throw new Error();
 	});
 
 	it('can paginate and load the correct data successfully', async () => {
+		renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
 		throw new Error();
 	});
 });

f03402ffd3f8af75a4383f07c7e121fd76185c24
Craig Miller <craigmiller160@gmail.com>
2022-07-01 12:46:14 -0400

    Working on query string conversion

--- src/ajaxapi/service/TransactionService.ts
+++ src/ajaxapi/service/TransactionService.ts
@@ -1,1 +1,22 @@
-export {};
+import {
+	DATE_FORMAT,
+	SearchTransactionsRequest
+} from '../../types/transactions';
+import * as Time from '@craigmiller160/ts-functions/es/Time';
+import qs from 'qs';
+
+const formatSearchDate = Time.format(DATE_FORMAT);
+
+export const requestToQuery = (request: SearchTransactionsRequest): string =>
+	qs.stringify({
+		...request,
+		startDate: request.startDate
+			? formatSearchDate(request.startDate)
+			: undefined,
+		endDate: request.endDate ? formatSearchDate(request.endDate) : undefined
+	});
+
+// export const searchForTransactions = (
+// 	request: SearchTransactionsRequest
+// ): Promise<SearchTransactionsResponse> => {};
+//

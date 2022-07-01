d967c93d38a328c06125ca1a93efb1fc7ebc57f1
Craig Miller <craigmiller160@gmail.com>
2022-07-01 17:04:55 -0400

    Making progress with test logic

--- test/components/Content/Transactions/Transactions.test.tsx
+++ test/components/Content/Transactions/Transactions.test.tsx
@@ -4,6 +4,19 @@ import { screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import * as RNonEmptyArray from 'fp-ts/es6/ReadonlyNonEmptyArray';
 import { pipe } from 'fp-ts/es6/function';
+import * as Try from '@craigmiller160/ts-functions/es/Try';
+import { TryT, MonoidT } from '@craigmiller160/ts-functions/es/types';
+import * as Either from 'fp-ts/es6/Either';
+import { match } from 'ts-pattern';
+import * as Monoid from 'fp-ts/es6/Monoid';
+
+const validationMonoid: MonoidT<TryT<unknown>> = {
+	empty: Either.right(null),
+	concat: (try1, try2) =>
+		match(try1)
+			.when(Either.isRight, () => try2)
+			.otherwise(() => try1)
+};
 
 describe('Transactions', () => {
 	let apiServer: ApiServer;
@@ -34,13 +47,24 @@ describe('Transactions', () => {
 			expect(screen.queryByText('Rows per page:')).toBeVisible()
 		);
 
-		pipe(
+		const result = pipe(
 			RNonEmptyArray.range(0, 25),
 			RNonEmptyArray.map((index) => `Transaction ${index}`),
 			RNonEmptyArray.map((description) =>
-				expect(screen.queryByText(description)).toBeVisible()
-			)
+				pipe(
+					Try.tryCatch(() =>
+						expect(screen.queryByText(description)).toBeVisible()
+					),
+					Either.mapLeft(
+						(ex) => new Error(`Error validating ${description}`, ex)
+					)
+				)
+			),
+			Monoid.concatAll(validationMonoid)
 		);
+		if (Either.isLeft<Error>(result)) {
+			throw result.left;
+		}
 	});
 
 	it('can change the rows-per-page and automatically re-load the data', async () => {

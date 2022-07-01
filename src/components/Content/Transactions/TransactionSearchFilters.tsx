1a98f846b33a91a6ba4e6632971d9946fa161761
Craig Miller <craigmiller160@gmail.com>
2022-07-01 13:59:40 -0400

    Fixed lint issues

--- src/components/Content/Transactions/TransactionSearchFilters.tsx
+++ src/components/Content/Transactions/TransactionSearchFilters.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable */
 import { useForm } from 'react-hook-form';
 
 interface TransactionSearchForm {
 	readonly direction: any; // TODO fix this for combobox option
 	readonly startDate: any; // TODO need a date picker
 	readonly endDate: any; // TODO need a date picker
-	readonly categoryType: any;// TODO need a combobox
+	readonly categoryType: any; // TODO need a combobox
 	readonly categories: any; // TODO need a combobox
 }
 

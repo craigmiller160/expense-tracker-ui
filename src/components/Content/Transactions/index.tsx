46f6050844e694f4d20290da3d9dbaadb08f60b4
Craig Miller <craigmiller160@gmail.com>
2022-07-01 12:32:10 -0400

    Added Transactions page

---
+++ src/components/Content/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import { PageTitle } from '../../UI/PageTitle';
+import './Transactions.scss';
+
+export const Transactions = () => {
+	return (
+		<div className="ManageTransactions">
+			<PageTitle title="Manage Transactions" />
+		</div>
+	);
+};

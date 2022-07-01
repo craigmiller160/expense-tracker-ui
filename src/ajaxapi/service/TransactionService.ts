b083cb624dbcc34af1b75abc25cbcfaddba8319b
Craig Miller <craigmiller160@gmail.com>
2022-07-01 12:57:01 -0400

    Cleaned up query param generation logic

--- src/ajaxapi/service/TransactionService.ts
+++ src/ajaxapi/service/TransactionService.ts
@@ -4,21 +4,29 @@ import {
 } from '../../types/transactions';
 import * as Time from '@craigmiller160/ts-functions/es/Time';
 import qs from 'qs';
+import { pipe } from 'fp-ts/es6/function';
+import * as Option from 'fp-ts/es6/Option';
 
 const formatSearchDate = Time.format(DATE_FORMAT);
+
+const handleOptionalValue = <T>(
+	value: T | undefined,
+	formatter: (v: T) => string
+): string | undefined =>
+	pipe(
+		Option.fromNullable(value),
+		Option.map(formatter),
+		Option.getOrElse((): string | undefined => undefined)
+	);
 
 export const requestToQuery = (request: SearchTransactionsRequest): string =>
 	qs.stringify({
 		...request,
-		startDate: request.startDate
-			? formatSearchDate(request.startDate)
-			: undefined,
-		endDate: request.endDate
-			? formatSearchDate(request.endDate)
-			: undefined,
-		categoryIds: request.categoryIds
-			? request.categoryIds.join(',')
-			: undefined
+		startDate: handleOptionalValue(request.startDate, formatSearchDate),
+		endDate: handleOptionalValue(request.endDate, formatSearchDate),
+		categoryIds: handleOptionalValue(request.categoryIds, (ids) =>
+			ids.join(',')
+		)
 	});
 
 // export const searchForTransactions = (

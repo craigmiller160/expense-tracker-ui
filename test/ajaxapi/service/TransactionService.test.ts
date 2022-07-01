0330317de897bcf648e87fb63f08a2d87a314c35
Craig Miller <craigmiller160@gmail.com>
2022-07-01 12:49:40 -0400

    Implemented first test

--- test/ajaxapi/service/TransactionService.test.ts
+++ test/ajaxapi/service/TransactionService.test.ts
@@ -1,9 +1,21 @@
+import { requestToQuery } from '../../../src/ajaxapi/service/TransactionService';
+import { SortDirection } from '../../../src/types/misc';
+import { TransactionSortKey } from '../../../src/types/transactions';
+
 export {};
 
 describe('TransactionService', () => {
 	describe('requestToQuery', () => {
 		it('only required fields', () => {
-			throw new Error();
+			const result = requestToQuery({
+				pageNumber: 1,
+				pageSize: 10,
+				sortDirection: SortDirection.ASC,
+				sortKey: TransactionSortKey.EXPENSE_DATE
+			});
+			expect(result).toEqual(
+				'pageNumber=1&pageSize=10&sortDirection=ASC&sortKey=EXPENSE_DATE'
+			);
 		});
 
 		it('all fields', () => {

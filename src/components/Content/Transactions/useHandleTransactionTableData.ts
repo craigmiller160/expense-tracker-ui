3a213fe3b94e40486047367be954bc2526e02327
Craig Miller <craigmiller160@gmail.com>
2022-07-02 13:27:28 -0400

    Continuing to build out new custom hook

--- src/components/Content/Transactions/useHandleTransactionTableData.ts
+++ src/components/Content/Transactions/useHandleTransactionTableData.ts
@@ -6,18 +6,42 @@ import {
 } from '../../../types/transactions';
 import { SortDirection } from '../../../types/misc';
 import { CategoryResponse } from '../../../types/categories';
+import { SelectOption } from '@craigmiller160/react-hook-form-material-ui';
+import { useMemo } from 'react';
+import { useForm, UseFormReturn } from 'react-hook-form';
+
+export type CategoryOption = SelectOption<string>;
 
 export interface PaginationState {
 	readonly pageNumber: number;
 	readonly pageSize: number;
 }
 
+export interface TransactionFormValues {
+	readonly category: CategoryOption;
+}
+
+export interface TransactionTableForm {
+	readonly transactions: ReadonlyArray<TransactionFormValues>;
+}
+
+const createTransactionFormKey = (index: number, field: string): string =>
+	`transactions.${index}.${field}`;
+
 export interface TransactionTableData {
 	readonly transactions: ReadonlyArray<TransactionResponse>;
-	readonly categories: ReadonlyArray<CategoryResponse>;
+	readonly categories: ReadonlyArray<CategoryOption>;
 	readonly totalRecords: number;
 	readonly isFetching: boolean;
+	readonly form: UseFormReturn<TransactionTableForm>;
 }
+
+const categoryToCategoryOption = (
+	category: CategoryResponse
+): CategoryOption => ({
+	label: category.name,
+	value: category.id
+});
 
 export const useHandleTransactionTableData = (
 	pagination: PaginationState
@@ -30,11 +54,18 @@ export const useHandleTransactionTableData = (
 			sortKey: TransactionSortKey.EXPENSE_DATE,
 			sortDirection: SortDirection.ASC
 		});
+	const form = useForm<TransactionTableForm>();
+
+	const categories = useMemo(
+		() => categoryData?.map(categoryToCategoryOption),
+		[categoryData]
+	);
 
 	return {
 		transactions: transactionData?.transactions ?? [],
-		categories: categoryData ?? [],
+		categories: categories ?? [],
 		totalRecords: transactionData?.totalItems ?? 0,
-		isFetching: transactionIsFetching || categoryIsFetching
+		isFetching: transactionIsFetching || categoryIsFetching,
+		form
 	};
 };

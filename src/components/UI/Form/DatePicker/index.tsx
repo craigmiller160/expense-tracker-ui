1b9c2d4cc5d1dfd20a36e30b920cb55b42add924
Craig Miller <craigmiller160@gmail.com>
2022-07-03 14:04:37 -0400

    Working on DatePicker

---
+++ src/components/UI/Form/DatePicker/index.tsx
@@ -0,0 +1,24 @@
+import { Control, FieldPath, FieldValues, Controller } from 'react-hook-form';
+import { Rules } from '@craigmiller160/react-hook-form-material-ui';
+import { DesktopDatePicker } from '@mui/x-date-pickers';
+import MuiTextField from '@mui/material/TextField';
+
+interface Props<F extends FieldValues> {
+	readonly name: FieldPath<F>;
+	readonly control: Control<F>;
+	readonly label: string;
+	readonly rules?: Rules<F>;
+}
+
+export const DatePicker = <F extends FieldValues>(props: Props<F>) => (
+	<Controller
+		name={props.name}
+		control={props.control}
+		render={({ field }) => (
+			<DesktopDatePicker
+				{...field}
+				renderInput={(params) => <MuiTextField {...params} />}
+			/>
+		)}
+	/>
+);

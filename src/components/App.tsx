eb3171655bb8a8e7e7751dfff88ad1d4bcba0572
Craig Miller <craigmiller160@gmail.com>
2022-07-03 12:09:08 -0400

    Added dependencies for date pickers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,15 +6,19 @@ import { AlertProvider } from './UI/Alerts/AlertProvider';
 import { AppQueryAndErrorHandlingProvider } from './QueryClient/AppQueryAndErrorHandlingProvider';
 import { ConfirmDialogProvider } from './UI/ConfirmDialog/ConfirmDialogProvider';
 import { ConfirmDialog } from './UI/ConfirmDialog';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 export const App = () => (
 	<AlertProvider>
 		<AppQueryAndErrorHandlingProvider>
 			<ConfirmDialogProvider>
 				<BrowserRouter basename="/">
-					<CssBaseline />
-					<Navbar />
-					<Content />
+					<LocalizationProvider dateAdapter={AdapterDateFns}>
+						<CssBaseline />
+						<Navbar />
+						<Content />
+					</LocalizationProvider>
 				</BrowserRouter>
 				<ConfirmDialog />
 			</ConfirmDialogProvider>

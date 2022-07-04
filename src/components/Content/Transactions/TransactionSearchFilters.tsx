16b665d21295151aa913ac27e59125971c5e3caf
Craig Miller <craigmiller160@gmail.com>
2022-07-04 18:56:13 -0400

    Refactoring the search filters

--- src/components/Content/Transactions/TransactionSearchFilters.tsx
+++ src/components/Content/Transactions/TransactionSearchFilters.tsx
@@ -7,20 +7,11 @@ import {
 } from '@craigmiller160/react-hook-form-material-ui';
 import { constVoid } from 'fp-ts/es6/function';
 import './TransactionSearchFilters.scss';
-import { TransactionCategoryType } from '../../../types/transactions';
 import { SortDirection } from '../../../types/misc';
 import { useGetAllCategories } from '../../../ajaxapi/query/CategoryQueries';
 import { useMemo } from 'react';
 import { categoryToCategoryOption, TransactionSearchForm } from './utils';
 import { Paper } from '@mui/material';
-
-const categorizationStatusOptions: ReadonlyArray<
-	SelectOption<TransactionCategoryType>
-> = [
-	{ value: TransactionCategoryType.ALL, label: 'All' },
-	{ value: TransactionCategoryType.WITH_CATEGORY, label: 'Categorized' },
-	{ value: TransactionCategoryType.WITHOUT_CATEGORY, label: 'Uncategorized' }
-];
 
 const directionOptions: ReadonlyArray<SelectOption<SortDirection>> = [
 	{ value: SortDirection.ASC, label: 'Oldest to Newest' },
@@ -46,41 +37,43 @@ export const TransactionSearchFilters = (props: Props) => {
 	return (
 		<Paper className="TransactionSearchFilters">
 			<form onSubmit={constVoid}>
-				<DatePicker
-					name="startDate"
-					control={control}
-					label="Start Date"
-					rules={{ required: 'Start Date is required' }}
-					dynamicSubmit={dynamicSubmit}
-				/>
-				<DatePicker
-					name="endDate"
-					control={control}
-					label="End Date"
-					rules={{ required: 'End Date is required' }}
-					dynamicSubmit={dynamicSubmit}
-				/>
-				<Select
-					name="categoryType"
-					control={control}
-					label="Categorization Status"
-					options={categorizationStatusOptions}
-					dynamicSubmit={dynamicSubmit}
-				/>
-				<Autocomplete
-					name="category"
-					control={control}
-					label="Category"
-					options={categoryOptions ?? []}
-					dynamicSubmit={dynamicSubmit}
-				/>
-				<Select
-					name="direction"
-					options={directionOptions}
-					control={control}
-					label="Order By"
-					dynamicSubmit={dynamicSubmit}
-				/>
+				<div className="row">
+					<DatePicker
+						name="startDate"
+						control={control}
+						label="Start Date"
+						rules={{ required: 'Start Date is required' }}
+						dynamicSubmit={dynamicSubmit}
+					/>
+					<DatePicker
+						name="endDate"
+						control={control}
+						label="End Date"
+						rules={{ required: 'End Date is required' }}
+						dynamicSubmit={dynamicSubmit}
+					/>
+				</div>
+				<div className="row">
+					<Autocomplete
+						name="category"
+						control={control}
+						label="Category"
+						options={categoryOptions ?? []}
+						dynamicSubmit={dynamicSubmit}
+					/>
+					<Select
+						name="direction"
+						options={directionOptions}
+						control={control}
+						label="Order By"
+						dynamicSubmit={dynamicSubmit}
+					/>
+				</div>
+				<div className="row">
+					<p>Is Duplicate</p>
+					<p>Is Not Confirmed</p>
+					<p>Is Not Categorized</p>
+				</div>
 			</form>
 		</Paper>
 	);

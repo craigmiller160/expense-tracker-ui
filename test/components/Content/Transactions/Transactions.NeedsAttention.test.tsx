a05b4805d13620bef88e91645cf28cbd1b1c8413
Craig Miller <craigmiller160@gmail.com>
2022-07-06 15:18:02 -0400

    Added test setup logic

--- test/components/Content/Transactions/Transactions.NeedsAttention.test.tsx
+++ test/components/Content/Transactions/Transactions.NeedsAttention.test.tsx
@@ -1,23 +1,81 @@
+import { ApiServer, newApiServer } from '../../../server';
+import { renderApp } from '../../../testutils/renderApp';
+import { screen, waitFor } from '@testing-library/react';
+
 export {};
 
 describe('Transactions Needs Attention', () => {
+	let apiServer: ApiServer;
+	beforeEach(() => {
+		apiServer = newApiServer();
+	});
+
+	afterEach(() => {
+		apiServer.server.shutdown();
+	});
+
 	it('has duplicates', async () => {
+		await renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
+		await waitFor(() =>
+			expect(screen.queryByText('Expense Tracker')).toBeVisible()
+		);
+		await waitFor(() =>
+			expect(screen.queryAllByText('Manage Transactions')).toHaveLength(2)
+		);
 		throw new Error();
 	});
 
 	it('has unconfirmed', async () => {
+		await renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
+		await waitFor(() =>
+			expect(screen.queryByText('Expense Tracker')).toBeVisible()
+		);
+		await waitFor(() =>
+			expect(screen.queryAllByText('Manage Transactions')).toHaveLength(2)
+		);
 		throw new Error();
 	});
 
 	it('has uncategorized', async () => {
+		await renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
+		await waitFor(() =>
+			expect(screen.queryByText('Expense Tracker')).toBeVisible()
+		);
+		await waitFor(() =>
+			expect(screen.queryAllByText('Manage Transactions')).toHaveLength(2)
+		);
 		throw new Error();
 	});
 
 	it('has all', async () => {
+		await renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
+		await waitFor(() =>
+			expect(screen.queryByText('Expense Tracker')).toBeVisible()
+		);
+		await waitFor(() =>
+			expect(screen.queryAllByText('Manage Transactions')).toHaveLength(2)
+		);
 		throw new Error();
 	});
 
 	it('has none', async () => {
+		await renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
+		await waitFor(() =>
+			expect(screen.queryByText('Expense Tracker')).toBeVisible()
+		);
+		await waitFor(() =>
+			expect(screen.queryAllByText('Manage Transactions')).toHaveLength(2)
+		);
 		throw new Error();
 	});
 });

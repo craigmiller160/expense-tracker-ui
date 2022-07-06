102ef94c8703719027c9290921bdcfe88a952466
Craig Miller <craigmiller160@gmail.com>
2022-07-06 15:34:48 -0400

    More work on test setup logic

--- test/components/Content/Transactions/Transactions.NeedsAttention.test.tsx
+++ test/components/Content/Transactions/Transactions.NeedsAttention.test.tsx
@@ -1,13 +1,85 @@
 import { ApiServer, newApiServer } from '../../../server';
 import { renderApp } from '../../../testutils/renderApp';
 import { screen, waitFor } from '@testing-library/react';
+import { Database } from '../../../server/Database';
+import * as Time from '@craigmiller160/ts-functions/es/Time';
+import { pipe } from 'fp-ts/es6/function';
+import { DATE_FORMAT } from '../../../../src/types/transactions';
+import * as RArray from 'fp-ts/es6/ReadonlyArray';
+import * as RNonEmptyArray from 'fp-ts/es6/ReadonlyNonEmptyArray';
+import * as Monoid from 'fp-ts/es6/Monoid';
+import {
+	createTransaction,
+	transactionRecordMonoid,
+	transactionToRecord
+} from '../../../testutils/transactionDataUtils';
 
-export {};
+const oldestDate = pipe(
+	new Date(),
+	Time.subDays(100),
+	Time.format(DATE_FORMAT)
+);
+
+interface Flags {
+	readonly notConfirmed: boolean;
+	readonly duplicate: boolean;
+	readonly notCategorized: boolean;
+}
+type PrepareData = (flags?: Partial<Flags>) => void;
+
+const createPrepareData =
+	(database: Database): PrepareData =>
+	(flags) => {
+		database.updateData((draft) => {
+			const firstCategory = Object.values(draft.categories)[0];
+			const baseTransactions = pipe(
+				Object.values(draft.transactions),
+				RArray.map((transaction) =>
+					createTransaction({
+						...transaction,
+						confirmed: false,
+						duplicate: false,
+						categoryId: firstCategory.id,
+						categoryName: firstCategory.name
+					})
+				),
+				RArray.map(transactionToRecord),
+				Monoid.concatAll(transactionRecordMonoid)
+			);
+			const newTransactions = pipe(
+				RNonEmptyArray.range(0, 2),
+				RArray.map((index) =>
+					createTransaction({
+						amount: 10 + index,
+						confirmed: flags?.notConfirmed ?? true,
+						duplicate: flags?.duplicate ?? false,
+						categoryId:
+							flags?.notCategorized ?? false
+								? undefined
+								: firstCategory.id,
+						categoryName:
+							flags?.notCategorized ?? false
+								? undefined
+								: firstCategory.name,
+						expenseDate: oldestDate
+					})
+				),
+				RArray.map(transactionToRecord),
+				Monoid.concatAll(transactionRecordMonoid)
+			);
+			draft.transactions = {
+				...baseTransactions,
+				...newTransactions
+			};
+		});
+	};
 
 describe('Transactions Needs Attention', () => {
 	let apiServer: ApiServer;
+	let prepareData: PrepareData;
 	beforeEach(() => {
 		apiServer = newApiServer();
+		prepareData = createPrepareData(apiServer.database);
 	});
 
 	afterEach(() => {

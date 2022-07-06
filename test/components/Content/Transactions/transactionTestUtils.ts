eb37868db3630cc84bf22a414a2ff21c7ec52268
Craig Miller <craigmiller160@gmail.com>
2022-07-06 15:06:46 -0400

    Moved functions to another file

--- test/components/Content/Transactions/transactionTestUtils.ts
+++ test/components/Content/Transactions/transactionTestUtils.ts
@@ -1,6 +1,13 @@
 import { screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as Time from '@craigmiller160/ts-functions/es/Time';
+import { TestTransactionDescription } from '../../../testutils/transactionDataUtils';
+import { TryT } from '@craigmiller160/ts-functions/es/types';
+import { flow, pipe } from 'fp-ts/es6/function';
+import * as Try from '@craigmiller160/ts-functions/es/Try';
+import * as Either from 'fp-ts/es6/Either';
+import * as Json from '@craigmiller160/ts-functions/es/Json';
+import * as RArray from 'fp-ts/es6/ReadonlyArray';
 
 export const getOrderByValueElement = (): HTMLElement | null | undefined => {
 	const transactionFilters = screen.getByTestId('transaction-filters');
@@ -51,3 +58,63 @@ export const getRecordRangeText = (): string | undefined =>
 
 export const getTotalDaysInRange = (startDate: Date, endDate: Date): number =>
 	Time.differenceInDays(endDate)(startDate) + 1;
+
+type ValidateDescription = (
+	index: number,
+	description: TestTransactionDescription
+) => void;
+
+const validateTransactionDescription =
+	(validateDescription: ValidateDescription) =>
+	(index: number, description: TestTransactionDescription): TryT<unknown> =>
+		pipe(
+			Try.tryCatch(() => validateDescription(index, description)),
+			Either.mapLeft(
+				(ex) =>
+					new Error(
+						`Error validating description ${JSON.stringify(
+							description
+						)}: ${ex.message}`,
+						{
+							cause: ex
+						}
+					)
+			)
+		);
+
+const validateNullableTextAndParse = (
+	descriptionElement: HTMLElement
+): TryT<TestTransactionDescription> => {
+	if (descriptionElement.textContent === null) {
+		return Either.left(
+			new Error('Description text content cannot be null')
+		);
+	}
+	return Json.parseE<TestTransactionDescription>(
+		descriptionElement.textContent
+	);
+};
+
+export const validateTransactionsInTable = (
+	count: number,
+	validateDescription: ValidateDescription
+) => {
+	const descriptions = screen.getAllByTestId('transaction-description');
+	expect(descriptions).toHaveLength(count);
+	const result = pipe(
+		descriptions,
+		RArray.map(validateNullableTextAndParse),
+		Either.sequenceArray,
+		Either.chain(
+			flow(
+				RArray.mapWithIndex(
+					validateTransactionDescription(validateDescription)
+				),
+				Either.sequenceArray
+			)
+		)
+	);
+	if (Either.isLeft(result)) {
+		throw result.left;
+	}
+};

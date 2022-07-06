08dc21b35f04c38b1d5da7fab18f5880f0bc47e2
Craig Miller <craigmiller160@gmail.com>
2022-07-06 10:54:03 -0400

    More tweaks to test

--- test/components/Content/Transactions/Transactions.test.tsx
+++ test/components/Content/Transactions/Transactions.test.tsx
@@ -522,7 +522,7 @@ describe('Transactions', () => {
 				Time.format(ARIA_LABEL_FORMAT)
 			);
 			await selectDate('Start Date', dateToSelect);
-			await Sleep.sleep(50)();
+			await Sleep.immediate()();
 			await waitFor(() =>
 				expect(screen.queryByText('Rows per page:')).toBeVisible()
 			);

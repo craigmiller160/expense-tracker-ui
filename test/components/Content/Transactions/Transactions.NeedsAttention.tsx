25081c5b7eceb0c793f98ea5dffe6876f52a023c
Craig Miller <craigmiller160@gmail.com>
2022-07-06 13:24:31 -0400

    Stubbed out tests

---
+++ test/components/Content/Transactions/Transactions.NeedsAttention.tsx
@@ -0,0 +1,19 @@
+export {};
+
+describe('Transactions Needs Attention', () => {
+	it('has duplicates', async () => {
+		throw new Error();
+	});
+
+	it('has unconfirmed', async () => {
+		throw new Error();
+	});
+
+	it('has uncategorized', async () => {
+		throw new Error();
+	});
+
+	it('has all', async () => {
+		throw new Error();
+	});
+});

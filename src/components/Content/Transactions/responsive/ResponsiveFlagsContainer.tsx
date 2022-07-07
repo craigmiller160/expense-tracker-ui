63841687f075c0fcc27d5211291da61b21580a3a
Craig Miller <craigmiller160@gmail.com>
2022-07-07 18:14:29 -0400

    Improved the layout of the flags

--- src/components/Content/Transactions/responsive/ResponsiveFlagsContainer.tsx
+++ src/components/Content/Transactions/responsive/ResponsiveFlagsContainer.tsx
@@ -3,6 +3,18 @@ import { InnerDivProps } from '../../../UI/ResponsiveWrappers/utils';
 import { addThemeToWrapper } from '../../../UI/ResponsiveWrappers/addThemeToWrapper';
 
 const InnerDiv = styled.div<InnerDivProps>`
+	display: flex;
+
+	${({ theme }) => theme.breakpoints.up('xs')} {
+		flex-direction: column;
+		align-items: center;
+	}
+
+	${({ theme }) => theme.breakpoints.up('md')} {
+		flex-direction: row;
+		justify-content: center;
+	}
+
 	> * {
 		margin: 0 0.2rem;
 		&:not(.visible) {

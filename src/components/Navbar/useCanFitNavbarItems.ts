8ba037bb7011c151481f58fa45959bbb8c1d2261
Craig Miller <craigmiller160@gmail.com>
2022-07-08 08:55:44 -0400

    Refactored breakpoint hook to make it more flexible

--- src/components/Navbar/useCanFitNavbarItems.ts
+++
@@ -1,6 +0,0 @@
-import { useMediaQuery, useTheme } from '@mui/material';
-
-export const useCanFitNavbarItems = (): boolean => {
-	const theme = useTheme();
-	return useMediaQuery(theme.breakpoints.up('md'));
-};

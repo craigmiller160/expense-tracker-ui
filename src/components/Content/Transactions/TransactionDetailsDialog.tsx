f10ded0def56a9ef22fae6b0b8de9e3ac3ca8c5b
Craig Miller <craigmiller160@gmail.com>
2022-07-23 14:46:18 -0400

    Added action buttons for dialog

--- src/components/Content/Transactions/TransactionDetailsDialog.tsx
+++ src/components/Content/Transactions/TransactionDetailsDialog.tsx
@@ -2,6 +2,9 @@ import { OptionT } from '@craigmiller160/ts-functions/es/types';
 import { TransactionResponse } from '../../../types/transactions';
 import { SideDialog } from '../../UI/SideDialog';
 import * as Option from 'fp-ts/es6/Option';
+import { Button } from '@mui/material';
+import { pipe } from 'fp-ts/es6/function';
+import './TransactionDetailsDialog.scss';
 
 interface Props {
 	readonly selectedTransaction: OptionT<TransactionResponse>;
@@ -10,14 +13,51 @@ interface Props {
 	readonly deleteTransaction: (id: string) => void;
 }
 
+interface DialogActionsProps {
+	readonly deleteTransaction: () => void;
+}
+
+const TransactionDetailsDialogActions = (props: DialogActionsProps) => (
+	<div className="TransactionDetailsActions">
+		<Button
+			variant="contained"
+			color="success"
+			disabled={true}
+			type="submit"
+		>
+			Save
+		</Button>
+		<Button
+			variant="contained"
+			color="error"
+			onClick={props.deleteTransaction}
+		>
+			Delete
+		</Button>
+	</div>
+);
+
 export const TransactionDetailsDialog = (props: Props) => {
 	// TODO consider different transaction details
 	const hasTransaction = Option.isSome(props.selectedTransaction);
+	const id = pipe(
+		props.selectedTransaction,
+		Option.map((txn) => txn.id),
+		Option.getOrElse(() => '')
+	);
+
+	const Actions = (
+		<TransactionDetailsDialogActions
+			deleteTransaction={() => props.deleteTransaction(id)}
+		/>
+	);
+
 	return (
 		<SideDialog
 			open={hasTransaction}
 			onClose={props.onClose}
 			title="Transaction Details"
+			actions={Actions}
 		>
 			<h1>Details Go Here</h1>
 		</SideDialog>

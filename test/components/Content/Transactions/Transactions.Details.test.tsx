bc40422a81a089166b2e7eca5ea7268524c117c7
Craig Miller <craigmiller160@gmail.com>
2022-07-23 14:38:11 -0400

    Stubbed out tests

---
+++ test/components/Content/Transactions/Transactions.Details.test.tsx
@@ -0,0 +1,19 @@
+export {};
+
+describe('Transaction Details Dialog', () => {
+	it('shows current transaction information', async () => {
+		throw new Error();
+	});
+
+	it('can confirm transaction', async () => {
+		throw new Error();
+	});
+
+	it('can categorize transaction', async () => {
+		throw new Error();
+	});
+
+	it('can delete transaction', async () => {
+		throw new Error();
+	});
+});

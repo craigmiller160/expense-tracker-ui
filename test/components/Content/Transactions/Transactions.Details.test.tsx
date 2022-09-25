b1493d6dd8fdadaeddbd201ecb567f4ba45754a7
Craig Miller <craigmiller160@gmail.com>
2022-09-25 15:55:16 -0400

    Got test to pass

--- test/components/Content/Transactions/Transactions.Details.test.tsx
+++ test/components/Content/Transactions/Transactions.Details.test.tsx
@@ -1,4 +1,22 @@
-export {};
+import { renderApp } from '../../../testutils/renderApp';
+import { screen, waitFor, within } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+
+const testButton =
+	(isDisabled: boolean) => (detailsButton: HTMLElement, index: number) => {
+		try {
+			if (isDisabled) {
+				expect(detailsButton).toBeDisabled();
+			} else {
+				expect(detailsButton).toBeEnabled();
+			}
+		} catch (ex) {
+			throw new Error(`Error testing details button at index ${index}`, {
+				cause: ex as Error
+			});
+		}
+	};
 
 describe('Transaction Details Dialog', () => {
 	it('shows current transaction information for unconfirmed and uncategorized', async () => {
@@ -24,6 +42,32 @@ describe('Transaction Details Dialog', () => {
 	});
 
 	it('cannot open details dialog when table form is dirty', async () => {
-		throw new Error();
+		await renderApp({
+			initialPath: '/expense-tracker/transactions'
+		});
+		await waitFor(() =>
+			expect(screen.queryByText('Expense Tracker')).toBeVisible()
+		);
+		await waitFor(() =>
+			expect(screen.queryAllByText('Manage Transactions')).toHaveLength(2)
+		);
+		await waitFor(() =>
+			expect(screen.queryByText('Rows per page:')).toBeVisible()
+		);
+		const row = screen.getAllByTestId('transaction-table-row')[0];
+		const confirmCheckbox = within(row).getByTestId(
+			'confirm-transaction-checkbox'
+		);
+
+		const detailsButtons = screen
+			.getAllByText('Details')
+			.filter((item) => item instanceof HTMLButtonElement);
+		expect(detailsButtons).toHaveLength(25);
+		detailsButtons.forEach(testButton(false));
+
+		await userEvent.click(confirmCheckbox);
+		expect(confirmCheckbox.querySelector('input')).toBeChecked();
+
+		detailsButtons.forEach(testButton(true));
 	});
 });

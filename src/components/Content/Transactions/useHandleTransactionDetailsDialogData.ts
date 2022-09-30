75526f48f251fabc1252406f20cfd6bad18ce009
Craig Miller <craigmiller160@gmail.com>
2022-09-30 14:48:47 -0400

    Revert "Heavily refactoring how data is setup for details dialog"
    This reverts commit 2a002ab0e64ff304ba1dc065bf8fd8871e85d746.

--- src/components/Content/Transactions/useHandleTransactionDetailsDialogData.ts
+++ src/components/Content/Transactions/useHandleTransactionDetailsDialogData.ts
@@ -3,52 +3,93 @@ import { TransactionResponse } from '../../../types/transactions';
 import { pipe } from 'fp-ts/es6/function';
 import * as Option from 'fp-ts/es6/Option';
 import { CategoryOption, transactionToCategoryOption } from './utils';
-import { useForm, UseFormReset, UseFormReturn } from 'react-hook-form';
+import { useForm, UseFormReturn } from 'react-hook-form';
 import { useEffect, useMemo } from 'react';
-import * as Time from '@craigmiller160/ts-functions/es/Time';
-
-const parseDate = Time.parse('yyyy-MM-dd');
 
 export type TransactionDetailsFormData = {
 	readonly confirmed: boolean;
 	readonly category: CategoryOption | null;
-	readonly amount: number;
-	readonly description: string;
-	readonly expenseDate: Date;
 };
 
-const createSetInitialFormValues =
-	(reset: UseFormReset<TransactionDetailsFormData>) =>
-	(transaction: OptionT<TransactionResponse>) => {
-		pipe(
-			transaction,
-			Option.map((txn) => {
-				reset({
-					confirmed: txn.confirmed,
-					category: transactionToCategoryOption(txn),
-					description: txn.description,
-					amount: txn.amount,
-					expenseDate: parseDate(txn.expenseDate)
-				});
-			})
-		);
-	};
+export type TransactionValues = {
+	readonly id: string | null;
+	readonly confirmed: boolean;
+	readonly duplicate: boolean;
+	readonly category: CategoryOption | null;
+	readonly expenseDate: string | null;
+	readonly description: string | null;
+	readonly amount: number;
+};
+
+export type TransactionDetailsDialogData = {
+	readonly transactionValues: TransactionValues;
+	readonly form: UseFormReturn<TransactionDetailsFormData>;
+};
+
+const useValuesFromSelectedTransaction = (
+	selectedTransaction: OptionT<TransactionResponse>
+): TransactionValues => {
+	// Doing this separately to help with dependency arrays
+	const transactionId: string | null = pipe(
+		selectedTransaction,
+		Option.map((txn): string | null => txn.id),
+		Option.getOrElse((): string | null => null)
+	);
+	return useMemo(
+		() =>
+			pipe(
+				selectedTransaction,
+				Option.map(
+					(transaction): TransactionValues => ({
+						id: transaction.id,
+						confirmed: transaction.confirmed,
+						duplicate: transaction.duplicate,
+						category: transactionToCategoryOption(transaction),
+						expenseDate: transaction.expenseDate,
+						description: transaction.description,
+						amount: transaction.amount
+					})
+				),
+				Option.getOrElse(
+					(): TransactionValues => ({
+						id: null,
+						confirmed: false,
+						duplicate: false,
+						category: null,
+						expenseDate: null,
+						description: null,
+						amount: 0
+					})
+				)
+			),
+		[transactionId] // eslint-disable-line react-hooks/exhaustive-deps
+	);
+};
 
 export const useHandleTransactionDetailsDialogData = (
 	selectedTransaction: OptionT<TransactionResponse>
-): UseFormReturn<TransactionDetailsFormData> => {
-	const form = useForm<TransactionDetailsFormData>();
+): TransactionDetailsDialogData => {
+	const transactionValues =
+		useValuesFromSelectedTransaction(selectedTransaction);
+
+	const form = useForm<TransactionDetailsFormData>({
+		defaultValues: {
+			confirmed: transactionValues.confirmed,
+			category: transactionValues.category
+		}
+	});
 
 	const { reset } = form;
-	const setInitialFormValues = useMemo(
-		() => createSetInitialFormValues(reset),
-		[reset]
-	);
 
 	useEffect(() => {
-		console.log('Setting form values');
-		setInitialFormValues(selectedTransaction);
-	}, [selectedTransaction, setInitialFormValues]);
+		reset({
+			confirmed: transactionValues.confirmed,
+			category: transactionValues.category
+		});
+	}, [transactionValues, reset]);
 
-	return form;
+	return {
+		transactionValues,
+		form
+	};
 };

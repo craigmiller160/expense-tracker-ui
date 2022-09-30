2a002ab0e64ff304ba1dc065bf8fd8871e85d746
Craig Miller <craigmiller160@gmail.com>
2022-09-30 14:15:53 -0400

    Heavily refactoring how data is setup for details dialog

--- src/components/Content/Transactions/useHandleTransactionDetailsDialogData.ts
+++ src/components/Content/Transactions/useHandleTransactionDetailsDialogData.ts
@@ -3,93 +3,52 @@ import { TransactionResponse } from '../../../types/transactions';
 import { pipe } from 'fp-ts/es6/function';
 import * as Option from 'fp-ts/es6/Option';
 import { CategoryOption, transactionToCategoryOption } from './utils';
-import { useForm, UseFormReturn } from 'react-hook-form';
+import { useForm, UseFormReset, UseFormReturn } from 'react-hook-form';
 import { useEffect, useMemo } from 'react';
+import * as Time from '@craigmiller160/ts-functions/es/Time';
+
+const parseDate = Time.parse('yyyy-MM-dd');
 
 export type TransactionDetailsFormData = {
 	readonly confirmed: boolean;
 	readonly category: CategoryOption | null;
-};
-
-export type TransactionValues = {
-	readonly id: string | null;
-	readonly confirmed: boolean;
-	readonly duplicate: boolean;
-	readonly category: CategoryOption | null;
-	readonly expenseDate: string | null;
-	readonly description: string | null;
 	readonly amount: number;
+	readonly description: string;
+	readonly expenseDate: Date;
 };
 
-export type TransactionDetailsDialogData = {
-	readonly transactionValues: TransactionValues;
-	readonly form: UseFormReturn<TransactionDetailsFormData>;
-};
-
-const useValuesFromSelectedTransaction = (
-	selectedTransaction: OptionT<TransactionResponse>
-): TransactionValues => {
-	// Doing this separately to help with dependency arrays
-	const transactionId: string | null = pipe(
-		selectedTransaction,
-		Option.map((txn): string | null => txn.id),
-		Option.getOrElse((): string | null => null)
-	);
-	return useMemo(
-		() =>
-			pipe(
-				selectedTransaction,
-				Option.map(
-					(transaction): TransactionValues => ({
-						id: transaction.id,
-						confirmed: transaction.confirmed,
-						duplicate: transaction.duplicate,
-						category: transactionToCategoryOption(transaction),
-						expenseDate: transaction.expenseDate,
-						description: transaction.description,
-						amount: transaction.amount
-					})
-				),
-				Option.getOrElse(
-					(): TransactionValues => ({
-						id: null,
-						confirmed: false,
-						duplicate: false,
-						category: null,
-						expenseDate: null,
-						description: null,
-						amount: 0
-					})
-				)
-			),
-		[transactionId] // eslint-disable-line react-hooks/exhaustive-deps
-	);
-};
+const createSetInitialFormValues =
+	(reset: UseFormReset<TransactionDetailsFormData>) =>
+	(transaction: OptionT<TransactionResponse>) => {
+		pipe(
+			transaction,
+			Option.map((txn) => {
+				reset({
+					confirmed: txn.confirmed,
+					category: transactionToCategoryOption(txn),
+					description: txn.description,
+					amount: txn.amount,
+					expenseDate: parseDate(txn.expenseDate)
+				});
+			})
+		);
+	};
 
 export const useHandleTransactionDetailsDialogData = (
 	selectedTransaction: OptionT<TransactionResponse>
-): TransactionDetailsDialogData => {
-	const transactionValues =
-		useValuesFromSelectedTransaction(selectedTransaction);
-
-	const form = useForm<TransactionDetailsFormData>({
-		defaultValues: {
-			confirmed: transactionValues.confirmed,
-			category: transactionValues.category
-		}
-	});
+): UseFormReturn<TransactionDetailsFormData> => {
+	const form = useForm<TransactionDetailsFormData>();
 
 	const { reset } = form;
+	const setInitialFormValues = useMemo(
+		() => createSetInitialFormValues(reset),
+		[reset]
+	);
 
 	useEffect(() => {
-		reset({
-			confirmed: transactionValues.confirmed,
-			category: transactionValues.category
-		});
-	}, [transactionValues, reset]);
+		console.log('Setting form values');
+		setInitialFormValues(selectedTransaction);
+	}, [selectedTransaction, setInitialFormValues]);
 
-	return {
-		transactionValues,
-		form
-	};
+	return form;
 };

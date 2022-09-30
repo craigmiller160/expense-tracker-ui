63c4226332b8bc2a7cd65c048bb23ddee0180690
Craig Miller <craigmiller160@gmail.com>
2022-09-30 16:43:51 -0400

    Implemented tests

--- test/components/Content/Transactions/Transactions.utils.test.ts
+++ test/components/Content/Transactions/Transactions.utils.test.ts
@@ -3,17 +3,29 @@ import { formatAmountValue } from '../../../../src/components/Content/Transactio
 describe('Transaction utils', () => {
 	describe('formatAmountValue', () => {
 		it('100', () => {
-			throw new Error();
+			const result = formatAmountValue('100');
+			expect(result).toEqual('100.00');
 		});
 		it('100.00', () => {
-			throw new Error();
+			const result = formatAmountValue('100.00');
+			expect(result).toEqual('100.00');
+		});
+		it('100.0000', () => {
+			const result = formatAmountValue('100.0000');
+			expect(result).toEqual('100.00');
+		});
+		it('100.0', () => {
+			const result = formatAmountValue('100.0');
+			expect(result).toEqual('100.00');
 		});
 		it('100.00.00', () => {
-			throw new Error();
+			const result = formatAmountValue('100.00.00');
+			expect(result).toEqual('100.00');
 		});
 
 		it('100abc', () => {
-			throw new Error();
+			const result = formatAmountValue('100abc');
+			expect(result).toEqual('100.00');
 		});
 	});
 });

5da0b9df8e2979466acc891a33c0e0345cbcf363
Craig Miller <craigmiller160@gmail.com>
2022-10-17 12:19:09 -0400

    Improving generate script

--- scripts/generateTypes.js
+++ scripts/generateTypes.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-console */
 const { generateApi } = require('swagger-typescript-api');
 const path = require('path');
-const axios = require('axios');
 
 generateApi({
 	name: 'expense-tracker',
 	output: path.join(process.cwd(), 'src', 'types', 'generated'),
 	url: 'https://127.0.0.1:8080/v3/api-docs',
 	prettier: true,
-	generateClient: false
+	generateClient: false,
+	sortTypes: true
 })
 	.then(() => console.log('API types successfully generated'))
 	.catch((ex) => console.error('Error generating API types', ex));

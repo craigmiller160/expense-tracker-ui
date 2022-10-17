eb678d8ae13cb700af51d0e0d85979a9e2ea8a58
Craig Miller <craigmiller160@gmail.com>
2022-10-17 08:14:12 -0400

    Working on generating types

---
+++ scripts/generateTypes.js
@@ -0,0 +1,12 @@
+const { generateApi } = require('swagger-typescript-api');
+const path = require('path');
+
+generateApi({
+	name: 'expense-tracker',
+	output: path.join(process.cwd(), 'src', 'types', 'generated.ts'),
+	url: 'https://locahost:8080/v3/api-docs',
+	prettier: true,
+	generateClient: false // TODO play with this
+})
+	.then(() => console.log('API types successfully generated'))
+	.catch((ex) => console.error('Error generating API types', ex));

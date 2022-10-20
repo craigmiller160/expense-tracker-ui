3a713472635fd896824e7ecbab7cffdc8cef4e45
Craig Miller <craigmiller160@gmail.com>
2022-10-20 14:48:43 -0400

    Output is stabilizing

--- scripts/generateTypes.js
+++ scripts/generateTypes.js
@@ -4,7 +4,13 @@ const path = require('path');
 
 const ARRAY_PATTERN = /^\((?<arrayType>.*)\)\[\]$/;
 
-const formatDataType = (parsedType) => {
+const formatDataType = (details) => {
+	if (details.enum) {
+		return details.enum.map((value) => `'${value}'`).join(' | ');
+	}
+
+	const parsedType = details.$parsed.content;
+
 	if (ARRAY_PATTERN.test(parsedType)) {
 		const arrayType = ARRAY_PATTERN.exec(parsedType).groups.arrayType;
 		return `ReadonlyArray<${arrayType}>`;
@@ -13,29 +19,38 @@ const formatDataType = (parsedType) => {
 	return parsedType;
 };
 
-const formatType = (type) => `
-	export type ${type.name} = {
-		${
-			type.properties &&
-			Object.entries(type.properties)
-				.map(
-					([name, details]) =>
-						`readonly ${name}: ${formatDataType(
-							details.$parsed.content
-						)};`
-				)
-				.join('\n')
-		}
-	};
+const formatProperties = (properties) => {
+	if (!properties) {
+		return '';
+	}
+	return Object.entries(properties)
+		.map(
+			([name, details]) => `readonly ${name}: ${formatDataType(details)};`
+		)
+		.join('\n\t');
+};
+
+// TODO handle if type is required or not
+const formatType = (type) => {
+	const properties = formatProperties(type.properties);
+	return `
+export type ${type.name} = {
+	${properties}
+};
 `;
+};
 
 const formatOutput = (res) =>
-	res.configuration.modelTypes.map(formatType).join('\n\n');
+	res.configuration.modelTypes
+		.filter((type) => type.name !== 'Unit')
+		.map(formatType)
+		.join('\n\n');
 
 const OUTPUT_PATH = path.join(process.cwd(), 'src', 'types', 'generated');
 
 generateApi({
 	name: 'expense-tracker',
+	// output: OUTPUT_PATH,
 	url: 'https://127.0.0.1:8080/v3/api-docs',
 	prettier: true,
 	generateClient: false,
--- src/types/generated/expense-tracker.ts
+++ src/types/generated/expense-tracker.ts
@@ -1,189 +1,138 @@
-
-	export type AuthCodeLoginDto = {
-		readonly url: string;
-	};
-
-
-
-	export type AuthUserDto = {
-		readonly userId: number;
-readonly username: string;
-readonly roles: ReadonlyArray<string>;
-readonly firstName: string;
-readonly lastName: string;
-	};
-
-
-
-	export type CategorizeTransactionsRequest = {
-		readonly transactionsAndCategories: ReadonlyArray<TransactionAndCategory>;
-	};
-
-
-
-	export type CategoryRequest = {
-		readonly name: string;
-	};
-
-
-
-	export type CategoryResponse = {
-		readonly id: string;
-readonly name: string;
-	};
-
-
-
-	export type ConfirmTransactionsRequest = {
-		readonly transactionsToConfirm: ReadonlyArray<TransactionToConfirm>;
-	};
-
-
-
-	export type CountAndOldest = {
-		readonly count: number;
-readonly oldest: string;
-	};
-
-
-
-	export type CreateTransactionRequest = {
-		readonly expenseDate: string;
-readonly description: string;
-readonly amount: number;
-readonly categoryId: string;
-	};
-
-
-
-	export type DeleteTransactionsRequest = {
-		readonly ids: ReadonlyArray<string>;
-	};
-
-
-
-	export type GetPossibleDuplicatesRequest = {
-		readonly pageNumber: number;
-readonly pageSize: number;
-	};
-
-
-
-	export type ImportTransactionsResponse = {
-		readonly transactionsImported: number;
-	};
-
-
-
-	export type ImportTypeResponse = {
-		readonly key: string;
-readonly displayName: string;
-	};
-
-
-
-	export type NeedsAttentionResponse = {
-		readonly unconfirmed: CountAndOldest;
-readonly uncategorized: CountAndOldest;
-readonly duplicate: CountAndOldest;
-readonly possibleRefund: CountAndOldest;
-	};
-
-
-
-	export type SearchTransactionsRequest = {
-		readonly pageNumber: number;
-readonly pageSize: number;
-readonly sortKey: [object Object];
-readonly sortDirection: [object Object],[object Object];
-readonly startDate: string;
-readonly endDate: string;
-readonly isConfirmed: boolean;
-readonly isCategorized: boolean;
-readonly isDuplicate: boolean;
-readonly isPossibleRefund: boolean;
-readonly categoryIds: ReadonlyArray<string>;
-	};
-
-
-
-	export type TransactionAndCategory = {
-		readonly transactionId: string;
-readonly categoryId: string;
-	};
-
-
-
-	export type TransactionDuplicatePageResponse = {
-		readonly transactions: ReadonlyArray<TransactionDuplicateResponse>;
-readonly pageNumber: number;
-readonly totalItems: number;
-	};
-
-
-
-	export type TransactionDuplicateResponse = {
-		readonly id: string;
-readonly created: string;
-readonly updated: string;
-readonly categoryName: string;
-	};
-
-
-
-	export type TransactionResponse = {
-		readonly id: string;
-readonly expenseDate: string;
-readonly description: string;
-readonly amount: number;
-readonly confirmed: boolean;
-readonly duplicate: boolean;
-readonly categoryId: string;
-readonly categoryName: string;
-	};
-
-
-
-	export type TransactionToConfirm = {
-		readonly transactionId: string;
-readonly confirmed: boolean;
-	};
-
-
-
-	export type TransactionToUpdate = {
-		readonly transactionId: string;
-readonly confirmed: boolean;
-readonly categoryId: string;
-	};
-
-
-
-	export type TransactionsPageResponse = {
-		readonly transactions: ReadonlyArray<TransactionResponse>;
-readonly pageNumber: number;
-readonly totalItems: number;
-	};
-
-
-
-	export type Unit = {
-		undefined
-	};
-
-
-
-	export type UpdateTransactionDetailsRequest = {
-		readonly transactionId: string;
-readonly confirmed: boolean;
-readonly expenseDate: string;
-readonly description: string;
-readonly amount: number;
-readonly categoryId: string;
-	};
-
-
-
-	export type UpdateTransactionsRequest = {
-		readonly transactions: ReadonlyArray<TransactionToUpdate>;
-	};
+export type AuthCodeLoginDto = {
+	readonly url: string;
+};
+
+export type AuthUserDto = {
+	readonly userId: number;
+	readonly username: string;
+	readonly roles: ReadonlyArray<string>;
+	readonly firstName: string;
+	readonly lastName: string;
+};
+
+export type CategorizeTransactionsRequest = {
+	readonly transactionsAndCategories: ReadonlyArray<TransactionAndCategory>;
+};
+
+export type CategoryRequest = {
+	readonly name: string;
+};
+
+export type CategoryResponse = {
+	readonly id: string;
+	readonly name: string;
+};
+
+export type ConfirmTransactionsRequest = {
+	readonly transactionsToConfirm: ReadonlyArray<TransactionToConfirm>;
+};
+
+export type CountAndOldest = {
+	readonly count: number;
+	readonly oldest: string;
+};
+
+export type CreateTransactionRequest = {
+	readonly expenseDate: string;
+	readonly description: string;
+	readonly amount: number;
+	readonly categoryId: string;
+};
+
+export type DeleteTransactionsRequest = {
+	readonly ids: ReadonlyArray<string>;
+};
+
+export type GetPossibleDuplicatesRequest = {
+	readonly pageNumber: number;
+	readonly pageSize: number;
+};
+
+export type ImportTransactionsResponse = {
+	readonly transactionsImported: number;
+};
+
+export type ImportTypeResponse = {
+	readonly key: string;
+	readonly displayName: string;
+};
+
+export type NeedsAttentionResponse = {
+	readonly unconfirmed: CountAndOldest;
+	readonly uncategorized: CountAndOldest;
+	readonly duplicate: CountAndOldest;
+	readonly possibleRefund: CountAndOldest;
+};
+
+export type SearchTransactionsRequest = {
+	readonly pageNumber: number;
+	readonly pageSize: number;
+	readonly sortKey: 'EXPENSE_DATE';
+	readonly sortDirection: 'ASC' | 'DESC';
+	readonly startDate: string;
+	readonly endDate: string;
+	readonly isConfirmed: boolean;
+	readonly isCategorized: boolean;
+	readonly isDuplicate: boolean;
+	readonly isPossibleRefund: boolean;
+	readonly categoryIds: ReadonlyArray<string>;
+};
+
+export type TransactionAndCategory = {
+	readonly transactionId: string;
+	readonly categoryId: string;
+};
+
+export type TransactionDuplicatePageResponse = {
+	readonly transactions: ReadonlyArray<TransactionDuplicateResponse>;
+	readonly pageNumber: number;
+	readonly totalItems: number;
+};
+
+export type TransactionDuplicateResponse = {
+	readonly id: string;
+	readonly created: string;
+	readonly updated: string;
+	readonly categoryName: string;
+};
+
+export type TransactionResponse = {
+	readonly id: string;
+	readonly expenseDate: string;
+	readonly description: string;
+	readonly amount: number;
+	readonly confirmed: boolean;
+	readonly duplicate: boolean;
+	readonly categoryId: string;
+	readonly categoryName: string;
+};
+
+export type TransactionToConfirm = {
+	readonly transactionId: string;
+	readonly confirmed: boolean;
+};
+
+export type TransactionToUpdate = {
+	readonly transactionId: string;
+	readonly confirmed: boolean;
+	readonly categoryId: string;
+};
+
+export type TransactionsPageResponse = {
+	readonly transactions: ReadonlyArray<TransactionResponse>;
+	readonly pageNumber: number;
+	readonly totalItems: number;
+};
+
+export type UpdateTransactionDetailsRequest = {
+	readonly transactionId: string;
+	readonly confirmed: boolean;
+	readonly expenseDate: string;
+	readonly description: string;
+	readonly amount: number;
+	readonly categoryId: string;
+};
+
+export type UpdateTransactionsRequest = {
+	readonly transactions: ReadonlyArray<TransactionToUpdate>;
+};

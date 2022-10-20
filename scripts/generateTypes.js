c71196d4a52d0fc176c43a4c2ed69ef7dd277c21
Craig Miller <craigmiller160@gmail.com>
2022-10-20 13:12:38 -0400

    Another attempt

--- scripts/generateTypes.js
+++ scripts/generateTypes.js
@@ -4,37 +4,38 @@ const path = require('path');
 const spawn = require('cross-spawn');
 
 // const result = spawn.sync(
-// 	'npx',
+// 	'swagger-typescript-api',
 // 	[
-// 		'swagger-typescript-api',
 // 		'-p',
 // 		'https://127.0.0.1:8080/v3/api-docs',
 // 		'-o',
 // 		path.join(process.cwd(), 'src', 'types', 'generated'),
 // 		'-n',
-// 		'expense-tracker',
-// 		'--union-enums',
-// 		'--no-client',
-// 		'--add-readonly '
+// 		'expense-tracker2'
+// 		// '--union-enums',
+// 		// '--no-client',
+// 		// '--add-readonly '
 // 	],
 // 	{
 // 		stdio: 'inherit',
+// 		cwd: process.cwd(),
 // 		env: {
 // 			NODE_TLS_REJECT_UNAUTHORIZED: 0
 // 		}
 // 	}
 // );
 // process.exit(result.status);
+// console.log(result);
 
 // TODO how to make this readonly?
 
-// generateApi({
-// 	name: 'expense-tracker',
-// 	output: path.join(process.cwd(), 'src', 'types', 'generated'),
-// 	url: 'https://127.0.0.1:8080/v3/api-docs',
-// 	prettier: true,
-// 	generateClient: false,
-// 	sortTypes: true
-// })
-// 	.then(() => console.log('API types successfully generated'))
-// 	.catch((ex) => console.error('Error generating API types', ex));
+generateApi({
+	name: 'expense-tracker',
+	output: path.join(process.cwd(), 'src', 'types', 'generated'),
+	url: 'https://127.0.0.1:8080/v3/api-docs',
+	prettier: true,
+	generateClient: false,
+	sortTypes: true
+})
+	.then(() => console.log('API types successfully generated'))
+	.catch((ex) => console.error('Error generating API types', ex));

82c32755ec849190ca06b9d0027cf518f4668156
Craig Miller <craigmiller160@gmail.com>
2022-11-07 15:27:38 -0500

    Finished details page factory

--- cypress/component/testutils/pages/categoryDetails.ts
+++ cypress/component/testutils/pages/categoryDetails.ts
@@ -4,8 +4,28 @@ const getHeaderTitle = (): Chainable<JQuery> =>
 	cy.get('#CategoryDetailsDialog .MuiToolbar-root h6');
 const getContentTitle = (): Chainable<JQuery> =>
 	cy.get('#CategoryDetailsDialog .MuiDialogContent-root h6');
+const getCategoryNameLabel = (): Chainable<JQuery> =>
+	cy.get('#CategoryDetailsDialog label').eq(0);
+const getCategoryNameInput = (): Chainable<JQuery> =>
+	getCategoryNameLabel()
+		.invoke('attr', 'for')
+		.then((forValue) => cy.get(`#${forValue}`));
+const getCloseButton = (): Chainable<JQuery> =>
+	cy.get('#CategoryDetailsDialog .MuiToolbar-root button');
+const getSaveButton = (): Chainable<JQuery> =>
+	cy.get(
+		'#CategoryDetailsDialog .MuiDialogActions-root button:nth-of-type(1)'
+	);
+const getDeleteButton = (): Chainable<JQuery> =>
+	cy.get(
+		'#CategoryDetailsDialog .MuiDialogActions-root button:nth-of-type(2)'
+	);
 
 export const categoryDetailsPage = {
 	getHeaderTitle,
-	getContentTitle
+	getContentTitle,
+	getCategoryNameInput,
+	getCloseButton,
+	getSaveButton,
+	getDeleteButton
 };

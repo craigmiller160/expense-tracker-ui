b248e23793c82afbd39f71f1ac202cce65517b15
Craig Miller <craigmiller160@gmail.com>
2022-11-07 12:46:52 -0500

    Integrated new mounting logic

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -1,16 +1,18 @@
 import { App } from '../../src/components/App';
 import { MemoryRouter } from 'react-router-dom';
+import { mountApp } from './testutils/mountApp';
 
 describe('App.cy.tsx', () => {
 	it('The whole app mounts', () => {
-		cy.viewport(1920, 1080);
-		cy.intercept('/expense-tracker/api/oauth/user', {
-			fixture: 'authorizedUser.json'
-		});
-		cy.mount(
-			<MemoryRouter initialEntries={['/expense-tracker']}>
-				<App />
-			</MemoryRouter>
-		);
+		// cy.viewport(1920, 1080);
+		// cy.intercept('/expense-tracker/api/oauth/user', {
+		// 	fixture: 'authorizedUser.json'
+		// });
+		// cy.mount(
+		// 	<MemoryRouter initialEntries={['/expense-tracker']}>
+		// 		<App />
+		// 	</MemoryRouter>
+		// );
+		mountApp();
 	});
 });

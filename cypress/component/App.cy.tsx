fb5965ad9d1519a58a245ffeab1c4672522d6c2a
Craig Miller <craigmiller160@gmail.com>
2022-11-07 12:24:23 -0500

    Got some intercepting and viewport stuff working

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -3,6 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 
 describe('App.cy.tsx', () => {
 	it('The whole app mounts', () => {
+		cy.viewport(1920, 1080);
 		cy.intercept('/expense-tracker/api/oauth/user', {
 			fixture: 'authorizedUser.json'
 		});

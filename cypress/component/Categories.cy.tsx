f121b30654c18191f47004879b16d6152281dd18
Craig Miller <craigmiller160@gmail.com>
2022-11-07 14:32:58 -0500

    Got more test logic working

--- cypress/component/Categories.cy.tsx
+++ cypress/component/Categories.cy.tsx
@@ -1,11 +1,28 @@
 import { getAllCategories } from './testutils/apis/categories';
 import { mountApp } from './testutils/mountApp';
 
+const CATEGORY_NAMES = ['Entertainment', 'Groceries', 'Restaurants', 'Travel'];
+
 describe('Manage Categories', () => {
 	it('displays all categories on the server', () => {
 		getAllCategories();
 		mountApp({
 			initialRoute: '/expense-tracker/categories'
 		});
+		cy.get('#navbar')
+			.find('.LinkButton')
+			.filter((index, $node) => $node.textContent === 'Manage Categories')
+			.should('have.class', 'active');
+		cy.get('.Categories').find('h4').contains('Manage Categories');
+		cy.get('.Categories table tr')
+			.should('have.length', 5)
+			.each(($node, index) => {
+				// Skip the header
+				if (index > 0) {
+					expect($node.children('td')).length(2);
+					const nameCell = $node.children('td').eq(0);
+					expect(nameCell.text()).to.eq(CATEGORY_NAMES[index - 1]);
+				}
+			});
 	});
 });

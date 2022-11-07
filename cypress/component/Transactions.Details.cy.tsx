93a4867835e1c5eefbade5256375dd89fa5bd55d
Craig Miller <craigmiller160@gmail.com>
2022-11-07 17:20:21 -0500

    Added the initial mounting to the test

--- cypress/component/Transactions.Details.cy.tsx
+++ cypress/component/Transactions.Details.cy.tsx
@@ -1,5 +1,10 @@
+import { mountApp } from './testutils/mountApp';
+
 describe('Transaction Details Dialog', () => {
 	it('input field validation rules work', () => {
+		mountApp({
+			initialRoute: '/expense-tracker/transactions'
+		});
 		throw new Error();
 	});
 });

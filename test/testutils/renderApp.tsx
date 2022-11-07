2f350113c348b410e0752db0eada889e9c9c7f49
Craig Miller <craigmiller160@gmail.com>
2022-11-07 16:55:09 -0500

    Trying to fix existing jest tests

--- test/testutils/renderApp.tsx
+++ test/testutils/renderApp.tsx
@@ -3,6 +3,7 @@ import { App } from '../../src/components/App';
 import * as Sleep from '@craigmiller160/ts-functions/es/Sleep';
 import * as Task from 'fp-ts/es6/Task';
 import { pipe } from 'fp-ts/es6/function';
+import { BrowserRouter } from 'react-router-dom';
 
 interface RenderConfig {
 	readonly initialPath?: string;
@@ -10,7 +11,11 @@ interface RenderConfig {
 
 export const renderApp = (config?: RenderConfig): Promise<RenderResult> => {
 	window.history.replaceState({}, '', config?.initialPath ?? '/');
-	const result = render(<App />);
+	const result = render(
+		<BrowserRouter basename="/">
+			<App />
+		</BrowserRouter>
+	);
 	return pipe(
 		Sleep.sleep(100),
 		Task.map(() => result)

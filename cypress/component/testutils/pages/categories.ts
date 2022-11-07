ecbe0bc4b41195f349744abaa8e4e577a73c60c5
Craig Miller <craigmiller160@gmail.com>
2022-11-07 15:02:45 -0500

    Fixed selector handling

--- cypress/component/testutils/pages/categories.ts
+++ cypress/component/testutils/pages/categories.ts
@@ -2,10 +2,7 @@ import Chainable = Cypress.Chainable;
 
 const getTitle = (): Chainable<JQuery> => cy.get('.Categories h4');
 const getTableRows = (): Chainable<JQuery> =>
-	cy
-		.get('.Categories table tr')
-		// Skip the header
-		.filter((index) => index > 0);
+	cy.get('.Categories table tbody tr');
 
 export const categoriesPage = {
 	getTitle,

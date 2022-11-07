934c5fa930848bb7e895469f0b326686ee5b5eff
Craig Miller <craigmiller160@gmail.com>
2022-11-07 12:23:09 -0500

    Making some progress

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -3,6 +3,9 @@ import { MemoryRouter } from 'react-router-dom';
 
 describe('App.cy.tsx', () => {
 	it('The whole app mounts', () => {
+		cy.intercept('/expense-tracker/api/oauth/user', {
+			fixture: 'authorizedUser.json'
+		});
 		cy.mount(
 			<MemoryRouter initialEntries={['/expense-tracker']}>
 				<App />

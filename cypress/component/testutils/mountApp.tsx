60ba279747b638b2a7470f593e1f26310864b7f8
Craig Miller <craigmiller160@gmail.com>
2022-11-07 12:45:55 -0500

    Fully implemented re-usable setup logic

--- cypress/component/testutils/mountApp.tsx
+++ cypress/component/testutils/mountApp.tsx
@@ -1,9 +1,11 @@
 import Chainable = Cypress.Chainable;
-import { match } from 'ts-pattern';
+import { match, P } from 'ts-pattern';
 import {
 	getAuthUser_isAuthorized,
 	getAuthUser_isNotAuthorized
 } from './apis/oauth';
+import { App } from '../../../src/components/App';
+import { MemoryRouter } from 'react-router-dom';
 
 const desktopViewport = (): Chainable<null> => cy.viewport(1920, 1080);
 const mobileViewport = (): Chainable<null> => cy.viewport(500, 500);
@@ -31,7 +33,19 @@ const handleIsAuthorized = (config?: Partial<MountConfig>): Chainable<null> =>
 		.with(undefined, getAuthUser_isAuthorized)
 		.with({ isAuthorized: true }, getAuthUser_isAuthorized)
 		.otherwise(getAuthUser_isNotAuthorized);
+const getInitialEntries = (config?: Partial<MountConfig>): string[] =>
+	match(config)
+		.with(undefined, () => ['/expense-tracker'])
+		.with({ initialRoute: P.string }, ({ initialRoute }) => [initialRoute])
+		.otherwise(() => ['/expense-tracker']);
 
-export const mountApp = (
-	config?: Partial<MountConfig>
-): Chainable<unknown> => {};
+export const mountApp = (config?: Partial<MountConfig>): Chainable<unknown> => {
+	handleViewport(config);
+	handleIsAuthorized(config);
+	const initialEntries = getInitialEntries(config);
+	return cy.mount(
+		<MemoryRouter initialEntries={initialEntries}>
+			<App />
+		</MemoryRouter>
+	);
+};

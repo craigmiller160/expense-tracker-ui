c8c56ba163442cda7e42b14ca55e8925ecebd193
Craig Miller <craigmiller160@gmail.com>
2022-11-08 10:32:21 -0500

    Added a lot more page factory functions

--- cypress/component/testutils/pages/transactionDetails.ts
+++ cypress/component/testutils/pages/transactionDetails.ts
@@ -1,6 +1,6 @@
 import Chainable = Cypress.Chainable;
 import { pipe } from 'fp-ts/es6/function';
-import { getInputForLabel } from './utils';
+import { getHelperTextForLabel, getInputForLabel } from './utils';
 
 const getHeaderTitle = (): Chainable<JQuery> =>
 	cy.get('#TransactionDetailsDialog-header .MuiToolbar-root h6');
@@ -20,10 +20,22 @@ const getCategorySelectLabel = (): Chainable<JQuery> =>
 	cy.get('#TransactionDetailsDialog-body .Controls label:nth-of-type(2)');
 const getExpenseDateLabel = (): Chainable<JQuery> =>
 	cy.get('#TransactionDetailsDialog-body .Info label:nth-of-type(1)');
+const getExpenseDateInput = (): Chainable<JQuery> =>
+	pipe(getExpenseDateLabel(), getInputForLabel);
+const getExpenseDateInputHelperText = (): Chainable<JQuery> =>
+	pipe(getExpenseDateLabel(), getHelperTextForLabel);
 const getAmountLabel = (): Chainable<JQuery> =>
 	cy.get('#TransactionDetailsDialog-body .Info label:nth-of-type(2)');
+const getAmountInput = (): Chainable<JQuery> =>
+	pipe(getAmountLabel(), getInputForLabel);
+const getAmountInputHelperText = (): Chainable<JQuery> =>
+	pipe(getAmountLabel(), getHelperTextForLabel);
 const getDescriptionLabel = (): Chainable<JQuery> =>
 	cy.get('#TransactionDetailsDialog-body .Info label:nth-of-type(3)');
+const getDescriptionInput = (): Chainable<JQuery> =>
+	pipe(getDescriptionLabel(), getInputForLabel);
+const getDescriptionHelperText = (): Chainable<JQuery> =>
+	pipe(getDescriptionLabel(), getHelperTextForLabel);
 
 export const transactionDetailsPage = {
 	getHeaderTitle,
@@ -36,5 +48,11 @@ export const transactionDetailsPage = {
 	getConfirmCheckboxInput,
 	getExpenseDateLabel,
 	getAmountLabel,
-	getDescriptionLabel
+	getDescriptionLabel,
+	getExpenseDateInput,
+	getExpenseDateInputHelperText,
+	getAmountInput,
+	getAmountInputHelperText,
+	getDescriptionInput,
+	getDescriptionHelperText
 };

1c6ce2579543cb8c5533debc4c4355f0de1427ff
Craig Miller <craigmiller160@gmail.com>
2022-11-08 11:27:14 -0500

    Added some test setup logic

--- cypress/component/Transactions.Table.cy.tsx
+++ cypress/component/Transactions.Table.cy.tsx
@@ -1,5 +1,15 @@
+import { categoriesApi } from './testutils/apis/categories';
+import { transactionsApi } from './testutils/apis/transactions';
+import { mountApp } from './testutils/mountApp';
+
 describe('Transactions Table', () => {
 	it('can reset in-progress changes on transactions', () => {
+		categoriesApi.getAllCategories();
+		transactionsApi.getNeedsAttention();
+		transactionsApi.searchForTransactions();
+		mountApp({
+			initialRoute: '/expense-tracker/transactions'
+		});
 		throw new Error();
 	});
 });

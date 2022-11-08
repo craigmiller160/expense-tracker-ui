b5488aa2f7925f97a902769fb8083fdab567df78
Craig Miller <craigmiller160@gmail.com>
2022-11-08 09:59:23 -0500

    A lot more work on test logic

--- cypress/component/Transactions.Details.cy.tsx
+++ cypress/component/Transactions.Details.cy.tsx
@@ -1,4 +1,6 @@
 import { mountApp } from './testutils/mountApp';
+import { transactionsApi } from './testutils/apis/transactions';
+import { allTransactions } from './testutils/constants/transactions';
 
 describe('Transaction Details Dialog', () => {
 	it('input field validation rules work', () => {
@@ -9,9 +11,12 @@ describe('Transaction Details Dialog', () => {
 	});
 
 	it('can confirm transaction', () => {
+		const transactionId = allTransactions[0].id;
+		transactionsApi.searchForTransactions();
+		transactionsApi.getTransactionDetails(transactionId);
 		mountApp({
 			initialRoute: '/expense-tracker/transactions'
 		});
-		throw new Error();
+		// TODO finish this
 	});
 });
--- cypress/component/testutils/apis/transactions.ts
+++ cypress/component/testutils/apis/transactions.ts
@@ -1,10 +1,23 @@
 import Chainable = Cypress.Chainable;
+import { TransactionDetailsResponse } from '../../../../src/types/generated/expense-tracker';
 
 const searchForTransactions = (): Chainable<null> =>
 	cy.intercept('/expense-tracker/api/transactions?*', {
 		fixture: 'allTransactions.json'
 	});
+const getTransactionDetails = (id: string): Chainable<null> =>
+	cy
+		.fixture('transactionDetails.json')
+		.then((fixture: TransactionDetailsResponse) =>
+			cy.intercept(`/expense-tracker/api/transactions/${id}/details`, {
+				fixture: {
+					...fixture,
+					id
+				}
+			})
+		);
 
 export const transactionsApi = {
-	searchForTransactions
+	searchForTransactions,
+	getTransactionDetails
 };

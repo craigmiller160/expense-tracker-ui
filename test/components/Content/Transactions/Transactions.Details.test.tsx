c9cdb14b7f75562f06ba727bced13a0d0331b1d8
Craig Miller <craigmiller160@gmail.com>
2022-11-08 11:15:58 -0500

    Deleted old test

--- test/components/Content/Transactions/Transactions.Details.test.tsx
+++ test/components/Content/Transactions/Transactions.Details.test.tsx
@@ -176,52 +176,6 @@ describe('Transaction Details Dialog', () => {
 		materialUiSelect('Category', transactionDialog).hasValue(category.name);
 	});
 
-	it('adds a new transaction', async () => {
-		// TODO this one is failing now... wtf...
-		await renderApp({
-			initialPath: '/expense-tracker/transactions'
-		});
-		await waitForVisibility([
-			{ text: 'Expense Tracker' },
-			{ text: 'Manage Transactions', occurs: 2, timeout: 3000 },
-			{ text: 'Rows per page:' }
-		]);
-
-		await userEvent.click(screen.getByText('Add Transaction'));
-
-		const transactionDialog = screen.getByTestId(
-			'transaction-details-dialog'
-		);
-
-		await waitFor(() => expect(transactionDialog).toBeVisible());
-
-		const replaceFieldValue = createReplaceFieldValue(transactionDialog);
-		await replaceFieldValue('Expense Date', '01/01/2500');
-		await replaceFieldValue('Amount ($)', '145.22');
-		await replaceFieldValue('Description', 'Hello World');
-
-		const select = materialUiSelect('Category', transactionDialog);
-		await select.selectItem('Groceries');
-		await select.hasValue('Groceries');
-
-		await userEvent.click(within(transactionDialog).getByText('Save'));
-
-		await waitForElementToBeRemoved(() =>
-			screen.queryByTestId('transaction-details-dialog')
-		);
-		await waitFor(() => screen.queryByTestId('table-loading'));
-		await waitFor(() =>
-			expect(screen.getAllByTestId('transaction-table-row')).toHaveLength(
-				25
-			)
-		);
-
-		const newTransaction = Object.values(
-			apiServer.database.data.transactions
-		).find((txn) => txn.expenseDate === '2500-01-01');
-		expect(newTransaction).not.toBeUndefined();
-	});
-
 	it('can update transaction information', async () => {
 		const {
 			transactions: [transaction]

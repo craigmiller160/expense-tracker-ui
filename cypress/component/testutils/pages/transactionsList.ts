8ad4909b1f7be925fcda871a9755f6f282bbc672
Craig Miller <craigmiller160@gmail.com>
2022-11-08 11:38:45 -0500

    Upgraded page factory for transaction list

--- cypress/component/testutils/pages/transactionsList.ts
+++ cypress/component/testutils/pages/transactionsList.ts
@@ -3,6 +3,12 @@ import Chainable = Cypress.Chainable;
 const getTitle = (): Chainable<JQuery> => cy.get('.ManageTransactions h4');
 const getDetailsButtons = (): Chainable<JQuery> =>
 	cy.get('.ManageTransactions table .DetailsButton');
+const getConfirmCheckboxes = (): Chainable<JQuery> =>
+	cy.get(
+		'.ManageTransactions table [data-testid="confirm-transaction-checkbox"]'
+	);
+const getCategorySelects = (): Chainable<JQuery> =>
+	cy.get('.ManageTransactions table .CategoryCell input');
 const getAddTransactionButton = (): Chainable<JQuery> =>
 	cy.get(
 		'.ManageTransactions .TransactionsTable .AboveTableActionWrapper button'
@@ -11,5 +17,7 @@ const getAddTransactionButton = (): Chainable<JQuery> =>
 export const transactionsListPage = {
 	getTitle,
 	getDetailsButtons,
-	getAddTransactionButton
+	getAddTransactionButton,
+	getConfirmCheckboxes,
+	getCategorySelects
 };

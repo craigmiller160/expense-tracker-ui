63a57060b7cf0e2caf6f9a45bcbfff1a186458c8
Craig Miller <craigmiller160@gmail.com>
2022-11-10 12:20:20 -0500

    Fixed broken tests

--- cypress/component/testutils/pages/categoriesList.ts
+++ cypress/component/testutils/pages/categoriesList.ts
@@ -4,9 +4,9 @@ const getTitle = (): Chainable<JQuery> => cy.get('.Categories h4');
 const getTableRows = (): Chainable<JQuery> =>
 	cy.get('.Categories table tbody tr');
 const getCategoryNames = (): Chainable<JQuery> =>
-	getTableRows().find('td:nth-of-type(1)');
+	getTableRows().find('td:nth-of-type(2)');
 const getDetailsButtons = (): Chainable<JQuery> =>
-	getTableRows().find('td:nth-of-type(2) button');
+	getTableRows().find('td:nth-of-type(3) button');
 const getAddButton = (): Chainable<JQuery> => cy.get('.ActionWrapper button');
 
 export const categoriesListPage = {

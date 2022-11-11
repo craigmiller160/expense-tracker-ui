68a416988bfbb659da81d4249defc52aa72ed065
Craig Miller <craigmiller160@gmail.com>
2022-11-11 13:09:52 -0500

    Beginning to implement logic

--- src/components/Content/Reports/ReportTable.tsx
+++ src/components/Content/Reports/ReportTable.tsx
@@ -1,20 +1,37 @@
-import { useGetSpendingByMonthAndCategory } from '../../../ajaxapi/query/ReportQueries';
-import { useImmer } from 'use-immer';
-import { Table } from '../../UI/Table';
+import {useGetSpendingByMonthAndCategory} from '../../../ajaxapi/query/ReportQueries';
+import {useImmer} from 'use-immer';
+import {Table} from '../../UI/Table';
+import {TableCell, TableRow} from '@mui/material';
+import {serverDateToReportMonth} from '../../../utils/dateTimeUtils';
 
 type State = {
 	readonly pageNumber: number;
 	readonly pageSize: number;
 };
 
-const COLUMNS = ['', ''];
+const COLUMNS = ['Month', 'Data', 'Chart'];
 
 export const ReportTable = () => {
 	const [state, setState] = useImmer<State>({
 		pageNumber: 0,
 		pageSize: 10
 	});
-	useGetSpendingByMonthAndCategory(state.pageNumber, state.pageSize);
+	const { isFetching, data } = useGetSpendingByMonthAndCategory(
+		state.pageNumber,
+		state.pageSize
+	);
 
-	return <Table columns={COLUMNS} />;
+	return (
+		<Table loading={isFetching} columns={COLUMNS}>
+			{data?.reports?.map((report) => (
+				<TableRow key={report.date}>
+					<TableCell>
+						{serverDateToReportMonth(report.date)}
+					</TableCell>
+					<TableCell></TableCell>
+					<TableCell></TableCell>
+				</TableRow>
+			))}
+		</Table>
+	);
 };

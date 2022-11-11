2887647cbc930a3d1787c785a0f63428c2ed1621
Craig Miller <craigmiller160@gmail.com>
2022-11-11 12:45:11 -0500

    Added reports tab

--- src/components/Navbar/items.ts
+++ src/components/Navbar/items.ts
@@ -9,8 +9,14 @@ export const MANAGE_CATEGORIES_TO = '/expense-tracker/categories';
 export const MANAGE_CATEGORIES_LABEL = 'Manage Categories';
 export const IMPORT_TRANSACTIONS_TO = '/expense-tracker/import';
 export const IMPORT_TRANSACTIONS_LABEL = 'Import Transactions';
+export const REPORTS_TO = '/expense-tracker/reports';
+export const REPORTS_LABEL = 'Reports';
 
 export const NAVBAR_ITEMS: ReadonlyArray<NavbarItem> = [
+	{
+		to: REPORTS_TO,
+		label: REPORTS_LABEL
+	},
 	{
 		to: MANAGE_TRANSACTIONS_TO,
 		label: MANAGE_TRANSACTIONS_LABEL

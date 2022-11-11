472db37e0b4f1d116edb90f84d33c31e45b1686d
Craig Miller <craigmiller160@gmail.com>
2022-11-11 12:46:01 -0500

    Stubbed out new tests

---
+++ cypress/component/Navigation.cy.tsx
@@ -0,0 +1,9 @@
+describe('Navigation', () => {
+	it('Desktop navbar takes user to all pages', () => {
+		throw new Error();
+	});
+
+	it('Mobile navbar takes user to all pages', () => {
+		throw new Error();
+	});
+});

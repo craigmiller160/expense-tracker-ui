bdc4a1dc730e73e84b85df423c18e9940bd69476
Craig Miller <craigmiller160@gmail.com>
2022-11-11 12:17:43 -0500

    Stubbed out base of reports page

---
+++ src/components/Content/Reports/index.tsx
@@ -0,0 +1,9 @@
+import { PageResponsiveWrapper } from '../../UI/ResponsiveWrappers/PageResponsiveWrapper';
+import { PageTitle } from '../../UI/PageTitle';
+import './Reports.scss';
+
+export const Reports = () => (
+	<PageResponsiveWrapper className="Reports">
+		<PageTitle title="Reports" />
+	</PageResponsiveWrapper>
+);

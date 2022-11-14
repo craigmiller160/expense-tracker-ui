2386bb360a4f104fc691342ee49d17e0fec76dac
Craig Miller <craigmiller160@gmail.com>
2022-11-14 14:17:29 -0500

    Added more intercepted apis

--- cypress/component/testutils/apis/needsAttention.ts
+++ cypress/component/testutils/apis/needsAttention.ts
@@ -7,6 +7,32 @@ const getNeedsAttention_all = (): Chainable<null> =>
 		})
 		.as('getNeedsAttention_all');
 
+const getNeedsAttention_none = (): Chainable<null> =>
+	cy.intercept('/expense-tracker/api/needs-attention', {
+		fixture: 'needsAttention_none.json'
+	});
+const getNeedsAttention_duplicates = (): Chainable<null> =>
+	cy.intercept('/expense-tracker/api/needs-attention', {
+		fixture: 'needsAttention_duplicates.json'
+	});
+const getNeedsAttention_possibleRefund = (): Chainable<null> =>
+	cy.intercept('/expense-tracker/api/needs-attention', {
+		fixture: 'needsAttention_possibleRefund.json'
+	});
+const getNeedsAttention_uncategorized = (): Chainable<null> =>
+	cy.intercept('/expense-tracker/api/needs-attention', {
+		fixture: 'needsAttention_uncategorized.json'
+	});
+const getNeedsAttention_unconfirmed = (): Chainable<null> =>
+	cy.intercept('/expense-tracker/api/needs-attention', {
+		fixture: 'needsAttention_unconfirmed.json'
+	});
+
 export const needsAttentionApi = {
-	getNeedsAttention_all
+	getNeedsAttention_all,
+	getNeedsAttention_none,
+	getNeedsAttention_duplicates,
+	getNeedsAttention_unconfirmed,
+	getNeedsAttention_uncategorized,
+	getNeedsAttention_possibleRefund
 };

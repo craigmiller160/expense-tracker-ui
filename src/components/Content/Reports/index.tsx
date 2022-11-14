cf9618d64369167e6b3d507508d7cafce0510776
Craig Miller <craigmiller160@gmail.com>
2022-11-14 12:51:01 -0500

    Added needs attention component to reports page

--- src/components/Content/Reports/index.tsx
+++ src/components/Content/Reports/index.tsx
@@ -2,10 +2,12 @@ import { PageResponsiveWrapper } from '../../UI/ResponsiveWrappers/PageResponsiv
 import { PageTitle } from '../../UI/PageTitle';
 import './Reports.scss';
 import { ReportTable } from './ReportTable';
+import { NeedsAttentionNotice } from '../Transactions/NeedsAttentionNotice';
 
 export const Reports = () => (
 	<PageResponsiveWrapper className="Reports">
 		<PageTitle title="Reports" />
+		<NeedsAttentionNotice />
 		<ReportTable />
 	</PageResponsiveWrapper>
 );

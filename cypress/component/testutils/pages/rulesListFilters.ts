27edf73758473997d3c5fcdbabf3e2580f1a2c4e
Craig Miller <craigmiller160@gmail.com>
2022-11-22 16:40:32 -0500

    More page factory functions

--- cypress/component/testutils/pages/rulesListFilters.ts
+++ cypress/component/testutils/pages/rulesListFilters.ts
@@ -1,11 +1,19 @@
 import Chainable = Cypress.Chainable;
+import { pipe } from 'fp-ts/es6/function';
+import { getInputForLabel } from './utils';
 
 const getRegexFilterLabel = (): Chainable<JQuery> =>
 	cy.get('.AutoCategorizeRulesFilters label').eq(0);
 const getCategoryFilterLabel = (): Chainable<JQuery> =>
 	cy.get('.AutoCategorizeRulesFilters label').eq(1);
+const getRegexFilterInput = (): Chainable<JQuery> =>
+	pipe(getRegexFilterLabel(), getInputForLabel);
+const getCategoryFilterInput = (): Chainable<JQuery> =>
+	pipe(getCategoryFilterLabel(), getInputForLabel);
 
 export const rulesListFiltersPage = {
 	getRegexFilterLabel,
-	getCategoryFilterLabel
+	getCategoryFilterLabel,
+	getRegexFilterInput,
+	getCategoryFilterInput
 };

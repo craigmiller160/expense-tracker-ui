caa37404f9a9b19f8f336eb711ea857873d022c4
Craig Miller <craigmiller160@gmail.com>
2022-11-22 12:08:50 -0500

    Finished new rule logic

--- cypress/component/Rules.Table.cy.tsx
+++ cypress/component/Rules.Table.cy.tsx
@@ -2,6 +2,24 @@ import { rulesApi } from './testutils/apis/rules';
 import { mountApp } from './testutils/mountApp';
 import { rulesListPage } from './testutils/pages/rulesList';
 import { allRules, columnNames } from './testutils/constants/rules';
+import { match } from 'ts-pattern';
+import { pipe } from 'fp-ts/es6/function';
+import * as Option from 'fp-ts/es6/Option';
+import { serverDateToDisplayDate } from '../../src/utils/dateTimeUtils';
+import { formatCurrency } from '../../src/utils/formatNumbers';
+
+const formatDate = (value?: string): string =>
+	pipe(
+		Option.fromNullable(value),
+		Option.map(serverDateToDisplayDate),
+		Option.getOrElse(() => 'Any')
+	);
+const formatAmount = (value?: number): string =>
+	pipe(
+		Option.fromNullable(value),
+		Option.map(formatCurrency),
+		Option.getOrElse(() => 'Any')
+	);
 
 const validateRuleRow = (row: JQuery, index: number) => {
 	rulesListPage
@@ -10,6 +28,30 @@ const validateRuleRow = (row: JQuery, index: number) => {
 	rulesListPage
 		.getCategoryCell(cy.wrap(row))
 		.should('have.text', allRules.rules[index].categoryName);
+	rulesListPage
+		.getRuleCell(cy.wrap(row))
+		.find('li')
+		.each(($li, liIndex) => {
+			const rule = allRules.rules[index];
+			const expectedText = match(liIndex)
+				.with(0, () => `Regex: /${rule.regex}/`)
+				.with(
+					1,
+					() =>
+						`Dates: ${formatDate(rule.startDate)} to ${formatDate(
+							rule.endDate
+						)}`
+				)
+				.with(
+					2,
+					() =>
+						`Amounts: ${formatAmount(
+							rule.minAmount
+						)} to ${formatAmount(rule.maxAmount)}`
+				)
+				.run();
+			expect($li.text()).eq(expectedText);
+		});
 };
 
 describe('Rules Table', () => {

ba7f80efa6de9644522ab7c075fd0280f3e65114
Craig Miller <craigmiller160@gmail.com>
2022-11-22 09:43:58 -0500

    Refactoring pagination logic to make it even more re-usable

---
+++ src/components/Content/Rules/RulesTable.tsx
@@ -0,0 +1,11 @@
+import { useGetAllRules } from '../../../ajaxapi/query/AutoCategorizeRuleQueries';
+import { PaginationState } from '../../../utils/pagination';
+
+type Props = {
+	readonly pagination: PaginationState;
+};
+
+export const RulesTable = () => {
+	// useGetAllRules()
+	return <div />;
+};
--- src/components/Content/Rules/index.tsx
+++ src/components/Content/Rules/index.tsx
@@ -1,9 +1,17 @@
 import './Rules.scss';
 import { PageResponsiveWrapper } from '../../UI/ResponsiveWrappers/PageResponsiveWrapper';
 import { PageTitle } from '../../UI/PageTitle';
+import { useImmer } from 'use-immer';
+import { PaginationState } from '../../../utils/pagination';
 
-export const Rules = () => (
-	<PageResponsiveWrapper className="AutoCategorizeRules">
-		<PageTitle title="Auto-Categorization Rules" />
-	</PageResponsiveWrapper>
-);
+export const Rules = () => {
+	const [state, setState] = useImmer<PaginationState>({
+		pageNumber: 0,
+		pageSize: 25
+	});
+	return (
+		<PageResponsiveWrapper className="AutoCategorizeRules">
+			<PageTitle title="Auto-Categorization Rules" />
+		</PageResponsiveWrapper>
+	);
+};

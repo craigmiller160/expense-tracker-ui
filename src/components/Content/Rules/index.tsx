13a14b28033d473e21b14981b103e8fddbdf26a3
Craig Miller <craigmiller160@gmail.com>
2022-11-22 12:28:26 -0500

    Integrated filters

--- src/components/Content/Rules/index.tsx
+++ src/components/Content/Rules/index.tsx
@@ -5,18 +5,26 @@ import { useImmer } from 'use-immer';
 import { PaginationState } from '../../../utils/pagination';
 import { RulesTable } from './RulesTable';
 import { useGetAllRulesData } from './useGetAllRulesData';
+import { RulesFilters } from './RulesFilters';
 
 export const Rules = () => {
 	const [state, setState] = useImmer<PaginationState>({
 		pageNumber: 0,
 		pageSize: 25
 	});
-	const { currentPage, totalItems, rules, isFetching } =
-		useGetAllRulesData(state);
+	const {
+		currentPage,
+		totalItems,
+		rules,
+		isFetching,
+		filtersForm,
+		categories
+	} = useGetAllRulesData(state);
 
 	return (
 		<PageResponsiveWrapper className="AutoCategorizeRules">
 			<PageTitle title="Auto-Categorization Rules" />
+			<RulesFilters form={filtersForm} categories={categories} />
 			<RulesTable
 				currentPage={currentPage}
 				totalItems={totalItems}

e76f9a8875a67d99d25e97d33652c96c97649c61
Craig Miller <craigmiller160@gmail.com>
2022-11-22 09:32:01 -0500

    Added new page for Rules

---
+++ src/components/Content/Rules/index.tsx
@@ -0,0 +1,9 @@
+import './Rules.scss';
+import { PageResponsiveWrapper } from '../../UI/ResponsiveWrappers/PageResponsiveWrapper';
+import { PageTitle } from '../../UI/PageTitle';
+
+export const Rules = () => (
+	<PageResponsiveWrapper className="AutoCategorizeRules">
+		<PageTitle title="Auto-Categorization Rules" />
+	</PageResponsiveWrapper>
+);

50d2af2ad90fd590c26c6af8acd4ab74ac19d2a2
Craig Miller <craigmiller160@gmail.com>
2022-11-23 15:32:53 -0500

    Implemented more intercepted functions

--- cypress/component/testutils/apis/rules.ts
+++ cypress/component/testutils/apis/rules.ts
@@ -17,8 +17,24 @@ const getRule_maximum = (id: string): Chainable<null> =>
 		fixture: 'ruleDetails_maximum.json'
 	});
 
+const createRule = (): Chainable<null> =>
+	cy
+		.intercept('post', '/expense-tracker/api/categories/rules', {
+			statusCode: 204
+		})
+		.as('createRule');
+
+const updateRule = (id: string): Chainable<null> =>
+	cy
+		.intercept('put', `/expense-tracker/api/categories/rules/${id}`, {
+			statusCode: 204
+		})
+		.as(`updateRule_${id}`);
+
 export const rulesApi = {
 	getAllRules,
 	getRule_minimum,
-	getRule_maximum
+	getRule_maximum,
+	createRule,
+	updateRule
 };

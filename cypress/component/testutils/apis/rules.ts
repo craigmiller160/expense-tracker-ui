a8556e4d535dadf24ae019e3d4b8e84f866f7a78
Craig Miller <craigmiller160@gmail.com>
2022-11-23 15:31:03 -0500

    Adding more intercepted API calls

--- cypress/component/testutils/apis/rules.ts
+++ cypress/component/testutils/apis/rules.ts
@@ -7,6 +7,18 @@ const getAllRules = (): Chainable<null> =>
 		})
 		.as('getAllRules');
 
+const getRule_minimum = (id: string): Chainable<null> =>
+	cy.intercept(`/expense-tracker/api/categories/rules/${id}`, {
+		fixture: 'ruleDetails_minimum.json'
+	});
+
+const getRule_maximum = (id: string): Chainable<null> =>
+	cy.intercept(`/expense-tracker/api/categories/rules/${id}`, {
+		fixture: 'ruleDetails_maximum.json'
+	});
+
 export const rulesApi = {
-	getAllRules
+	getAllRules,
+	getRule_minimum,
+	getRule_maximum
 };

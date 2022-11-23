cc73c8fd176a9cf1dcd95f0771c9aa6d530caae5
Craig Miller <craigmiller160@gmail.com>
2022-11-23 16:27:10 -0500

    Another phase of the refactor complete

--- cypress/component/Transactions.Details.cy.tsx
+++ cypress/component/Transactions.Details.cy.tsx
@@ -14,29 +14,13 @@ import {
 	serverDateTimeToDisplayDateTime,
 	serverDateToDisplayDate
 } from '../../src/utils/dateTimeUtils';
-import Chainable = Cypress.Chainable;
 import { commonPage } from './testutils/pages/common';
 import {
 	orderedCategoryIds,
 	orderedCategoryNames
 } from './testutils/constants/categories';
 import { validateInputRules } from './testutils/validations/inputRules';
-
-// TODO delete this
-const testValidationRule = (
-	input: Chainable<JQuery>,
-	getHelperText: () => Chainable<JQuery>,
-	errorMessage: string,
-	updatedValue: string
-) => {
-	input.clear();
-	transactionDetailsPage.getSaveButton().should('be.disabled');
-	getHelperText().contains(errorMessage);
-
-	input.type(updatedValue);
-	getHelperText().should('not.exist');
-	transactionDetailsPage.getSaveButton().should('not.be.disabled');
-};
+import Chainable = Cypress.Chainable;
 
 const testDuplicate = (getRecord: () => Chainable<JQuery>, index: number) => {
 	transactionDetailsPage
@@ -427,28 +411,33 @@ describe('Transaction Details Dialog', () => {
 		});
 
 		transactionsListPage.getDetailsButtons().eq(0).click();
-		validateInputRules({
+		const validateInput = validateInputRules({
+			getSaveButton: transactionDetailsPage.getSaveButton
+		});
+		validateInput({
 			getInput: transactionDetailsPage.getExpenseDateInput,
+			getHelperText: transactionDetailsPage.getExpenseDateInputHelperText
+		})({
 			invalidValue: '',
 			validValue: '01/02/2022',
-			getHelperText: transactionDetailsPage.getExpenseDateInputHelperText,
-			errorMessage: 'Expense Date is required',
-			getSaveButton: transactionDetailsPage.getSaveButton
+			errorMessage: 'Expense Date is required'
 		});
-		validateInputRules({
+		validateInput({
 			getInput: transactionDetailsPage.getAmountInput,
+			getHelperText: transactionDetailsPage.getAmountInputHelperText
+		})({
 			invalidValue: '',
 			validValue: '-10.00',
-			errorMessage: 'Amount is required',
-			getHelperText: transactionDetailsPage.getAmountInputHelperText,
-			getSaveButton: transactionDetailsPage.getSaveButton
+			errorMessage: 'Amount is required'
 		});
-		testValidationRule(
-			transactionDetailsPage.getDescriptionInput(),
-			transactionDetailsPage.getDescriptionHelperText,
-			'Description is required',
-			'Hello World'
-		);
+		validateInput({
+			getInput: transactionDetailsPage.getDescriptionInput,
+			getHelperText: transactionDetailsPage.getDescriptionHelperText
+		})({
+			errorMessage: 'Description is required',
+			validValue: 'Hello World',
+			invalidValue: ''
+		});
 	});
 
 	it('can categorize transaction', () => {
--- cypress/component/testutils/validations/inputRules.ts
+++ cypress/component/testutils/validations/inputRules.ts
@@ -1,30 +1,30 @@
 import Chainable = Cypress.Chainable;
 
-type Config = {
-	readonly getInput: () => Chainable<JQuery>;
+type PageConfig = {
 	readonly getSaveButton: () => Chainable<JQuery>;
+};
+type InputConfig = {
+	readonly getInput: () => Chainable<JQuery>;
 	readonly getHelperText: () => Chainable<JQuery>;
+};
+type RuleConfig = {
 	readonly errorMessage: string;
 	readonly validValue: string;
 	readonly invalidValue: string;
 };
 
-export const validateInputRules = ({
-	getInput,
-	getSaveButton,
-	getHelperText,
-	errorMessage,
-	validValue,
-	invalidValue
-}: Config) => {
-	getInput().clear();
-	if (invalidValue) {
-		getInput().type(invalidValue);
-	}
-	getSaveButton().should('be.disabled');
-	getHelperText().contains(errorMessage);
+export const validateInputRules =
+	({ getSaveButton }: PageConfig) =>
+	({ getInput, getHelperText }: InputConfig) =>
+	({ errorMessage, validValue, invalidValue }: RuleConfig) => {
+		getInput().clear();
+		if (invalidValue) {
+			getInput().type(invalidValue);
+		}
+		getSaveButton().should('be.disabled');
+		getHelperText().contains(errorMessage);
 
-	getInput().type(validValue);
-	getHelperText().should('not.exist');
-	getSaveButton().should('not.be.disabled');
-};
+		getInput().type(validValue);
+		getHelperText().should('not.exist');
+		getSaveButton().should('not.be.disabled');
+	};

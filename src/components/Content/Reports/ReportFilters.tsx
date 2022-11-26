071aabe14e221d7a350dd0f9f7b0a6b59654ff77
Craig Miller <craigmiller160@gmail.com>
2022-11-26 14:18:01 -0500

    Cleanup

--- src/components/Content/Reports/ReportFilters.tsx
+++ src/components/Content/Reports/ReportFilters.tsx
@@ -1,11 +1,10 @@
 import { UseFormReturn } from 'react-hook-form';
-import { CategoryOption } from '../../../types/categories';
 import { Paper } from '@mui/material';
 import { constVoid } from 'fp-ts/es6/function';
 import { ValueHasChanged } from '@craigmiller160/react-hook-form-material-ui';
 import { ResponsiveRow } from '../../UI/ResponsiveWrappers/ResponsiveRow';
 import './ReportFilters.scss';
-import {ReportFilterFormData} from './useGetReportData';
+import { ReportFilterFormData } from './useGetReportData';
 
 type Props = {
 	readonly form: UseFormReturn<ReportFilterFormData>;
@@ -16,9 +15,7 @@ export const ReportFilters = (props: Props) => {
 	return (
 		<Paper className="ReportFilters">
 			<form onSubmit={constVoid}>
-				<ResponsiveRow>
-
-				</ResponsiveRow>
+				<ResponsiveRow></ResponsiveRow>
 			</form>
 		</Paper>
 	);

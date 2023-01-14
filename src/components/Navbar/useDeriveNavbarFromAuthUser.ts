e442854b8667a54e39b2b4178c208f8781040346
Craig Miller <craigmiller160@gmail.com>
2023-01-14 12:39:12 -0500

    Fixed the navbar setup

--- src/components/Navbar/useDeriveNavbarFromAuthUser.ts
+++ src/components/Navbar/useDeriveNavbarFromAuthUser.ts
@@ -1,13 +1,5 @@
-import { match } from 'ts-pattern';
-import { useGetAuthUser } from '../../ajaxapi/query/AuthQueries';
-import { QueryStatus } from 'react-query';
-import { login, logout } from '../../ajaxapi/service/AuthService';
-
-interface DerivedFromAuthUser<T> {
-	readonly loading: T;
-	readonly failed: T;
-	readonly succeeded: T;
-}
+import { useContext } from 'react';
+import { KeycloakAuthContext } from '../KeycloakAuthProvider';
 
 interface DerivedValues {
 	readonly authButtonText: string;
@@ -16,38 +8,16 @@ interface DerivedValues {
 	readonly hasCheckedAuthorization: boolean;
 }
 
-const deriveFromAuthUser =
-	<T>(derived: DerivedFromAuthUser<T>) =>
-	(status: QueryStatus): T =>
-		match(status)
-			.with('error', () => derived.failed)
-			.with('success', () => derived.succeeded)
-			.otherwise(() => derived.loading);
-
-const getAuthButtonText = deriveFromAuthUser({
-	loading: '',
-	failed: 'Login',
-	succeeded: 'Logout'
-});
-
-const getAuthButtonAction = (refetch: () => Promise<unknown>) =>
-	deriveFromAuthUser<() => Promise<unknown>>({
-		loading: () => Promise.resolve(),
-		failed: login,
-		succeeded: () => logout().then(refetch)
-	});
-
 export const useDeriveNavbarFromAuthUser = (): DerivedValues => {
-	const {
-		result: { status, refetch },
-		extra: { isAuthorized, hasCheckedAuthorization }
-	} = useGetAuthUser();
-	const authButtonText = getAuthButtonText(status);
-	const authButtonAction = getAuthButtonAction(refetch)(status);
+	const { isAuthorized, hasCheckedAuthorization } =
+		useContext(KeycloakAuthContext);
+	const authButtonText = 'Logout';
+	// const authButtonAction = getAuthButtonAction(refetch)(status);
 
 	return {
 		authButtonText,
-		authButtonAction,
+		// TODO fix this for logout
+		authButtonAction: () => Promise.resolve(),
 		isAuthorized,
 		hasCheckedAuthorization
 	};

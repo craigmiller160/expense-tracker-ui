a3c866424ce74e7f29e4ef0ffc2ada727ece9224
Craig Miller <craigmiller160@gmail.com>
2023-01-14 12:01:37 -0500

    Setup terraform

---
+++ deploy/terraform/clients.tf
@@ -0,0 +1,32 @@
+data "keycloak_realm" "apps_dev" {
+  realm = "apps-dev"
+}
+
+data "keycloak_realm" "apps_prod" {
+  realm = "apps-prod"
+}
+
+locals {
+  client_common = {
+    client_id = "expense-tracker-ui"
+    name = "expense-tracker-ui"
+    enabled = true
+    access_type = "PUBLIC"
+  }
+}
+
+resource "keycloak_openid_client" "expense_tracker_ui_dev" {
+  realm_id = data.keycloak_realm.apps_dev.id
+  client_id = local.client_common.client_id
+  name = local.client_common.name
+  enabled = local.client_common.enabled
+  access_type = local.client_common.access_type
+}
+
+resource "keycloak_openid_client" "expense_tracker_ui_prod" {
+  realm_id = data.keycloak_realm.apps_prod.id
+  client_id = local.client_common.client_id
+  name = local.client_common.name
+  enabled = local.client_common.enabled
+  access_type = local.client_common.access_type
+}

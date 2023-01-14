f1fdf42105e6d692606b55a22e1a5a0df3b2fdfc
Craig Miller <craigmiller160@gmail.com>
2023-01-14 12:40:30 -0500

    Removing some unauthorized handling

--- src/components/QueryClient/QueryErrorSupportHandler.tsx
+++ src/components/QueryClient/QueryErrorSupportHandler.tsx
@@ -1,20 +1,15 @@
 import { PropsWithChildren, useContext, useEffect } from 'react';
 import { QueryErrorSupportContext } from './QueryErrorSupportProvider';
-import { useGetAuthUser } from '../../ajaxapi/query/AuthQueries';
 
 export const QueryErrorSupportHandler = (props: PropsWithChildren) => {
 	const { hasUnauthorizedError, setHasUnauthorizedError } = useContext(
 		QueryErrorSupportContext
 	);
-	const {
-		result: { refetch }
-	} = useGetAuthUser();
 	useEffect(() => {
 		if (hasUnauthorizedError) {
-			refetch();
 			setHasUnauthorizedError(false);
 		}
-	}, [hasUnauthorizedError, setHasUnauthorizedError, refetch]);
+	}, [hasUnauthorizedError, setHasUnauthorizedError]);
 
 	return <>{props.children}</>;
 };

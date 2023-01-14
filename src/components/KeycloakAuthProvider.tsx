5d0379c144dc2e3373747eeb4cf05314817815cf
Craig Miller <craigmiller160@gmail.com>
2023-01-14 12:26:47 -0500

    Implementing more provider logic

--- src/components/KeycloakAuthProvider.tsx
+++ src/components/KeycloakAuthProvider.tsx
@@ -1,10 +1,17 @@
 import { createContext, PropsWithChildren, useEffect } from 'react';
 import Keycloak from 'keycloak-js';
 import { BEARER_TOKEN_KEY } from '@craigmiller160/ajax-api';
+import { Updater, useImmer } from 'use-immer';
 
-export type KeycloakAuth = Record<string, never>;
+export type KeycloakAuth = {
+	readonly isAuthorized: boolean;
+	readonly hasCheckedAuthorization: boolean;
+};
 
-const KeycloakAuthContext = createContext<KeycloakAuth>({});
+const KeycloakAuthContext = createContext<KeycloakAuth>({
+	isAuthorized: false,
+	hasCheckedAuthorization: false
+});
 
 const ACCESS_TOKEN_EXP_SECS = 300;
 
@@ -15,35 +22,47 @@ const keycloak = new Keycloak({
 	clientId: 'expense-tracker-ui'
 });
 
-const handleKeycloakResult = (isSuccess: boolean) => {
-	if (isSuccess && keycloak.token) {
-		localStorage.setItem(BEARER_TOKEN_KEY, keycloak.token);
-	} else {
-		// TODO how to handle this?
-	}
-};
+const handleKeycloakResult =
+	(updateAuth: Updater<KeycloakAuth>) => (isSuccess: boolean) => {
+		if (isSuccess && keycloak.token) {
+			localStorage.setItem(BEARER_TOKEN_KEY, keycloak.token);
+		} else {
+			console.error('NO SUCCESS');
+			// TODO how to handle this?
+		}
+		updateAuth((draft) => {
+			draft.hasCheckedAuthorization = true;
+			draft.isAuthorized = isSuccess;
+		});
+	};
 
-const initializeKeycloak = (): Promise<void> => {
+const initializeKeycloak = (
+	updateAuth: Updater<KeycloakAuth>
+): Promise<void> => {
 	// TODO what about catch() in all of these?
 	const promise = keycloak
 		.init({ onLoad: 'login-required' })
-		.then(handleKeycloakResult);
+		.then(handleKeycloakResult(updateAuth));
 
 	setInterval(() => {
 		keycloak
 			.updateToken(ACCESS_TOKEN_EXP_SECS - 70)
-			.then(handleKeycloakResult);
+			.then(handleKeycloakResult(updateAuth));
 	}, (ACCESS_TOKEN_EXP_SECS - 60) * 1000);
 
 	return promise;
 };
 
 export const KeycloakAuthProvider = (props: PropsWithChildren) => {
+	const [state, setState] = useImmer<KeycloakAuth>({
+		isAuthorized: false,
+		hasCheckedAuthorization: false
+	});
 	useEffect(() => {
-		initializeKeycloak();
-	}, []);
+		initializeKeycloak(setState);
+	}, [setState]);
 	return (
-		<KeycloakAuthContext.Provider value={{}}>
+		<KeycloakAuthContext.Provider value={state}>
 			{props.children}
 		</KeycloakAuthContext.Provider>
 	);

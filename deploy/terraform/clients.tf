cbb01b902846640abfb51b10cd2d77dc932a652c
Craig Miller <craigmiller160@gmail.com>
2023-01-15 17:55:35 -0500

    Updated terraform config

--- deploy/terraform/clients.tf
+++ deploy/terraform/clients.tf
@@ -24,8 +24,7 @@ resource "keycloak_openid_client" "expense_tracker_ui_dev" {
   access_type = local.client_common.access_type
   standard_flow_enabled = local.client_common.standard_flow_enabled
   valid_redirect_uris = [
-    "https://localhost:3002/expense-tracker",
-    "https://localhost:3002/expense-tracker/"
+    "https://localhost:3002/expense-tracker/*"
   ]
   web_origins = [
     "https://localhost:3002"
@@ -40,8 +39,7 @@ resource "keycloak_openid_client" "expense_tracker_ui_prod" {
   access_type = local.client_common.access_type
   standard_flow_enabled = local.client_common.standard_flow_enabled
   valid_redirect_uris = [
-    "https://apps-craigmiller160.ddns.net/expense-tracker",
-    "https://apps-craigmiller160.ddns.net/expense-tracker/"
+    "https://apps-craigmiller160.ddns.net/expense-tracker/*"
   ]
   web_origins = [
     "https://apps-craigmiller160.ddns.net/expense-tracker"

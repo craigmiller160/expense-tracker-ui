8eb74925b05387bffa1ae8f80cac8f3e59324dcc
Craig Miller <craigmiller160@gmail.com>
2023-01-15 18:26:04 -0500

    Updated terraform script for logout

--- deploy/terraform/clients.tf
+++ deploy/terraform/clients.tf
@@ -26,6 +26,9 @@ resource "keycloak_openid_client" "expense_tracker_ui_dev" {
   valid_redirect_uris = [
     "https://localhost:3002/expense-tracker/*"
   ]
+  valid_post_logout_redirect_uris = [
+    "https://localhost:3002/expense-tracker/*"
+  ]
   web_origins = [
     "https://localhost:3002"
   ]
@@ -39,6 +42,9 @@ resource "keycloak_openid_client" "expense_tracker_ui_prod" {
   access_type = local.client_common.access_type
   standard_flow_enabled = local.client_common.standard_flow_enabled
   valid_redirect_uris = [
+    "https://apps-craigmiller160.ddns.net/expense-tracker/*"
+  ]
+  valid_post_logout_redirect_uris = [
     "https://apps-craigmiller160.ddns.net/expense-tracker/*"
   ]
   web_origins = [

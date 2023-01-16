b6000b09acc5e17913d8a828ed41d3ceccd11776
Craig Miller <craigmiller160@gmail.com>
2023-01-15 20:17:07 -0500

    Integrated KeycloakAuthContext into legacy jest tests

--- test/testutils/renderApp.tsx
+++ test/testutils/renderApp.tsx
@@ -4,16 +4,28 @@ import * as Sleep from '@craigmiller160/ts-functions/es/Sleep';
 import * as Task from 'fp-ts/es6/Task';
 import { pipe } from 'fp-ts/es6/function';
 import { BrowserRouter } from 'react-router-dom';
+import {
+	KeycloakAuth,
+	KeycloakAuthContext
+} from '../../src/components/keycloak/KeycloakAuthContext';
 
 interface RenderConfig {
 	readonly initialPath?: string;
 }
 
+const keycloakAuth: KeycloakAuth = {
+	isAuthorized: true,
+	checkStatus: 'post-check',
+	logout: jest.fn()
+};
+
 export const renderApp = (config?: RenderConfig): Promise<RenderResult> => {
 	window.history.replaceState({}, '', config?.initialPath ?? '/');
 	const result = render(
 		<BrowserRouter basename="/">
-			<App />
+			<KeycloakAuthContext.Provider value={keycloakAuth}>
+				<App />
+			</KeycloakAuthContext.Provider>
 		</BrowserRouter>
 	);
 	return pipe(

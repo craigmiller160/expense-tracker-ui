7349ac7500c166d40c2b38dff58baf9dd2722607
Craig Miller <craigmiller160@gmail.com>
2023-01-30 15:02:17 -0500

    Progress making things work

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+	root: path.join(process.cwd(), 'src'),
 	plugins: [react()]
 });

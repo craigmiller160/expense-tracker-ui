4201539d1c91efcb197780dd7547a525960abacb
Craig Miller <craigmiller160@gmail.com>
2023-02-11 12:13:17 -0500

    Improved the new alert handling with timeouts

--- src/components/UI/Alerts/index.tsx
+++ src/components/UI/Alerts/index.tsx
@@ -2,33 +2,53 @@ import { Alert, Stack } from '@mui/material';
 import './Alerts.scss';
 import { AlertData } from './types';
 import { useImmer } from 'use-immer';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { alertManager } from './AlertManager';
+
+const ALERT_TIMEOUT = 6_000;
 
 type State = {
 	readonly alerts: ReadonlyArray<AlertData>;
+	readonly timeouts: ReadonlyArray<[string, number]>;
 };
 
 export const Alerts = () => {
 	const [state, setState] = useImmer<State>({
-		alerts: []
+		alerts: [],
+		timeouts: []
 	});
 
+	const removeAlert = useCallback(
+		(id: string) =>
+			setState((draft) => {
+				const alertIndex = draft.alerts.findIndex(
+					(alert) => alert.id === id
+				);
+				if (alertIndex >= 0) {
+					draft.alerts.splice(alertIndex, 1);
+				}
+
+				const timeoutIndex = draft.timeouts.findIndex(
+					([alertId]) => alertId === id
+				);
+				if (timeoutIndex >= 0) {
+					clearTimeout(timeoutIndex);
+				}
+			}),
+		[setState]
+	);
+
 	useEffect(() => {
-		return alertManager.subscribe((alert) =>
+		return alertManager.subscribe((alert) => {
+			const timeout = window.setTimeout(() => {
+				removeAlert(alert.id);
+			}, ALERT_TIMEOUT);
 			setState((draft) => {
 				draft.alerts.push(alert);
-			})
-		);
-	}, [setState]);
-
-	const removeAlert = (id: string) =>
-		setState((draft) => {
-			const index = draft.alerts.findIndex((alert) => alert.id === id);
-			if (index >= 0) {
-				draft.alerts.splice(index, 1);
-			}
+				draft.timeouts.push([alert.id, timeout]);
+			});
 		});
+	}, [setState, removeAlert]);
 
 	return (
 		<div className="Alerts">

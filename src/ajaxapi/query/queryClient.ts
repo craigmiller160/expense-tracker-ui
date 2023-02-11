2fdc0fdf7687c35a808205733eee1d6f6a9538ac
Craig Miller <craigmiller160@gmail.com>
2023-02-11 11:59:25 -0500

    Moved all query client code to another file

---
+++ src/ajaxapi/query/queryClient.ts
@@ -0,0 +1,64 @@
+import { ErrorResponse, isErrorResponse } from '../../types/error';
+import { isAxiosError } from '@craigmiller160/ajax-api';
+import { alertManager } from '../../components/UI/Alerts/AlertManager';
+import { QueryClient } from '@tanstack/react-query';
+
+type CombinedErrorValues = {
+	readonly message: string;
+	readonly status?: number;
+	readonly errorResponse?: ErrorResponse;
+};
+
+// TODO move to file
+const getCombinedErrorValues = (error: Error): CombinedErrorValues => {
+	const messages: string[] = [];
+	let baseError: Error | undefined = error;
+	while (baseError != undefined) {
+		messages.push(baseError.message);
+		if (isAxiosError(baseError)) {
+			const status = baseError.response?.status ?? -1;
+			const body = baseError.response?.data;
+			const errorResponse: ErrorResponse | undefined = isErrorResponse(
+				body
+			)
+				? body
+				: undefined;
+			return {
+				message: messages.join('; '),
+				status,
+				errorResponse
+			};
+		}
+		baseError = baseError.cause as Error | undefined;
+	}
+	return {
+		message: messages.join('; ')
+	};
+};
+
+const onError = (error: unknown) => {
+	if (error instanceof Error) {
+		const combinedValues = getCombinedErrorValues(error);
+		if (combinedValues.status === 400 && !!combinedValues.errorResponse) {
+			alertManager.addAlert(
+				'error',
+				combinedValues.errorResponse.message
+			);
+		} else {
+			alertManager.addAlert('error', combinedValues.message);
+		}
+	}
+};
+
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			cacheTime: 0,
+			onError
+		},
+		mutations: {
+			onError
+		}
+	}
+});
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,70 +5,8 @@ import { ConfirmDialogProvider } from './UI/ConfirmDialog/ConfirmDialogProvider'
 import { ConfirmDialog } from './UI/ConfirmDialog';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { alertManager } from './UI/Alerts/AlertManager';
-import { isAxiosError } from '@craigmiller160/ajax-api';
-import { ErrorResponse, isErrorResponse } from '../types/error';
-
-type CombinedErrorValues = {
-	readonly message: string;
-	readonly status?: number;
-	readonly errorResponse?: ErrorResponse;
-};
-
-// TODO move to file
-const getCombinedErrorValues = (error: Error): CombinedErrorValues => {
-	const messages: string[] = [];
-	let baseError: Error | undefined = error;
-	while (baseError != undefined) {
-		messages.push(baseError.message);
-		if (isAxiosError(baseError)) {
-			const status = baseError.response?.status ?? -1;
-			const body = baseError.response?.data;
-			const errorResponse: ErrorResponse | undefined = isErrorResponse(
-				body
-			)
-				? body
-				: undefined;
-			return {
-				message: messages.join('; '),
-				status,
-				errorResponse
-			};
-		}
-		baseError = baseError.cause as Error | undefined;
-	}
-	return {
-		message: messages.join('; ')
-	};
-};
-
-const onError = (error: unknown) => {
-	if (error instanceof Error) {
-		const combinedValues = getCombinedErrorValues(error);
-		if (combinedValues.status === 400 && !!combinedValues.errorResponse) {
-			alertManager.addAlert(
-				'error',
-				combinedValues.errorResponse.message
-			);
-		} else {
-			alertManager.addAlert('error', combinedValues.message);
-		}
-	}
-};
-
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false,
-			cacheTime: 0,
-			onError
-		},
-		mutations: {
-			onError
-		}
-	}
-});
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../ajaxapi/query/queryClient';
 
 export const App = () => (
 	<QueryClientProvider client={queryClient}>

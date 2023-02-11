5f1509133c16fbd8ac9eefedf113fadc2506d02a
Craig Miller <craigmiller160@gmail.com>
2023-02-11 11:51:56 -0500

    Refactoring alerts component

--- src/components/UI/Alerts/index.tsx
+++ src/components/UI/Alerts/index.tsx
@@ -1,17 +1,31 @@
 import { Alert, Stack } from '@mui/material';
 import './Alerts.scss';
-import { useContext } from 'react';
-import { AlertContext } from './AlertProvider';
+import { AlertData } from './types';
+import { useImmer } from 'use-immer';
+
+type State = {
+	readonly alerts: ReadonlyArray<AlertData>;
+};
 
 export const Alerts = () => {
-	const alertContext = useContext(AlertContext);
+	const [state, setState] = useImmer<State>({
+		alerts: []
+	});
+
+	const removeAlert = (id: string) =>
+		setState((draft) => {
+			const index = draft.alerts.findIndex((alert) => alert.id === id);
+			if (index >= 0) {
+				draft.alerts.splice(index, 1);
+			}
+		});
 
 	return (
 		<div className="Alerts">
 			<Stack sx={{ width: '50%' }}>
-				{alertContext.alerts.map((alert) => (
+				{state.alerts.map((alert) => (
 					<Alert
-						onClose={() => alertContext.removeAlert(alert.id)}
+						onClose={() => removeAlert(alert.id)}
 						key={alert.id}
 						severity={alert.severity}
 					>

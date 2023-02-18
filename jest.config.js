ae7a5bb10448e670b360ac2b68a669e32a1df9e6
Craig Miller <craigmiller160@gmail.com>
2023-02-18 11:36:53 -0500

    Moving jest to swc

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,14 @@ const jestConfig = require('@craigmiller160/jest-config');
 const jestTsConfig = require('@craigmiller160/jest-config-ts');
 const path = require('path');
 
-module.exports = merge(jestConfig, jestTsConfig, {
-    setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
+// TODO update the jest-config lib
+const modifiedJestConfig = {
+	...jestConfig,
+	transform: {
+		'^.+\\.jsx?$': '@swc/jest'
+	}
+};
+
+module.exports = merge(modifiedJestConfig, jestTsConfig, {
+	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
 });

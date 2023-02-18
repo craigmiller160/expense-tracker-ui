067b42bcd1bf5d3b78533295b603af0476c5eaed
Craig Miller <craigmiller160@gmail.com>
2023-02-18 12:29:50 -0500

    Feature/swc (#60)
    * Switched react from babel to swc
    
    * Moving jest to swc
    
    * Jest swc works
    
    * Removing babel
    
    * Updating jest ts rules
    
    * Trying to make swc work
    
    * Cleanup
    
    * Got swc working for all jest tests
    
    * Progress
    
    * Adjusted tsconfig
    
    * Cleanup
    
    * More swc improvements
    
    * Switching to new jest config library
    
    * Lots of jest fixes
    
    * Got everything working
    
    * Cleanup
    
    * Updated lib
    
    * Cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 const merge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
-const jestTsConfig = require('@craigmiller160/jest-config-ts');
 const path = require('path');
 
-module.exports = merge(jestConfig, jestTsConfig, {
-    setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
+module.exports = merge(jestConfig, {
+	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
 });

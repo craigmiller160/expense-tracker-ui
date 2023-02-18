3bcfb84d964eb977a3d7d8b106b9740d73ab0694
Craig Miller <craigmiller160@gmail.com>
2023-02-18 11:37:52 -0500

    Jest swc works

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const jestTsConfig = require('@craigmiller160/jest-config-ts');
 const path = require('path');
 
 // TODO update the jest-config lib
+// TODO remove @swc/jest from this project and add to jest lib
 const modifiedJestConfig = {
 	...jestConfig,
 	transform: {

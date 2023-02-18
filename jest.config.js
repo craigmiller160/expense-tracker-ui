cdbbc3e27d56248a4c583f20a3b85da135593fd9
Craig Miller <craigmiller160@gmail.com>
2023-02-18 12:17:06 -0500

    Switching to new jest config library

--- jest.config.js
+++ jest.config.js
@@ -1,57 +1,56 @@
 const merge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
-const jestTsConfig = require('@craigmiller160/jest-config-ts');
 const path = require('path');
 
 // TODO update the jest-config lib
 // TODO remove @swc/jest from this project and add to jest lib
-const modifiedJestConfig = {
-	...jestConfig,
-	transform: {
-		'^.+\\.js$': '@swc/jest',
-		'^.+\\.ts$': [
-			'@swc/jest',
-			{
-				jsc: {
-					syntax: 'typescript'
-				}
-			}
-		],
-		'^.+\\.jsx$': [
-			'@swc/jest',
-			{
-				jsc: {
-					transform: {
-						react: {
-							runtime: 'automatic'
-						}
-					}
-				}
-			}
-		],
-		'^.+\\.tsx$': [
-			'@swc/jest',
-			{
-				jsc: {
-					parser: {
-						syntax: 'typescript',
-						tsx: true
-					},
-					transform: {
-						react: {
-							runtime: 'automatic'
-						}
-					}
-				}
-			}
-		]
-	}
-};
+// const modifiedJestConfig = {
+// 	...jestConfig,
+// 	transform: {
+// 		'^.+\\.js$': '@swc/jest',
+// 		'^.+\\.ts$': [
+// 			'@swc/jest',
+// 			{
+// 				jsc: {
+// 					syntax: 'typescript'
+// 				}
+// 			}
+// 		],
+// 		'^.+\\.jsx$': [
+// 			'@swc/jest',
+// 			{
+// 				jsc: {
+// 					transform: {
+// 						react: {
+// 							runtime: 'automatic'
+// 						}
+// 					}
+// 				}
+// 			}
+// 		],
+// 		'^.+\\.tsx$': [
+// 			'@swc/jest',
+// 			{
+// 				jsc: {
+// 					parser: {
+// 						syntax: 'typescript',
+// 						tsx: true
+// 					},
+// 					transform: {
+// 						react: {
+// 							runtime: 'automatic'
+// 						}
+// 					}
+// 				}
+// 			}
+// 		]
+// 	}
+// };
 
 // TODO update the jest-config-ts lib
-delete jestTsConfig.transform;
-delete jestTsConfig.globals;
+// delete jestTsConfig.transform;
+// delete jestTsConfig.globals;
 
-module.exports = merge(modifiedJestConfig, jestTsConfig, {
+module.exports = merge(jestConfig, {
 	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
 });

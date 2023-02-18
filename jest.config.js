4aa22a512c37acc02e07a49313d8539610e9eb6e
Craig Miller <craigmiller160@gmail.com>
2023-02-18 12:22:34 -0500

    Cleanup

--- jest.config.js
+++ jest.config.js
@@ -2,55 +2,6 @@ const merge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
 const path = require('path');
 
-// TODO update the jest-config lib
-// TODO remove @swc/jest from this project and add to jest lib
-// const modifiedJestConfig = {
-// 	...jestConfig,
-// 	transform: {
-// 		'^.+\\.js$': '@swc/jest',
-// 		'^.+\\.ts$': [
-// 			'@swc/jest',
-// 			{
-// 				jsc: {
-// 					syntax: 'typescript'
-// 				}
-// 			}
-// 		],
-// 		'^.+\\.jsx$': [
-// 			'@swc/jest',
-// 			{
-// 				jsc: {
-// 					transform: {
-// 						react: {
-// 							runtime: 'automatic'
-// 						}
-// 					}
-// 				}
-// 			}
-// 		],
-// 		'^.+\\.tsx$': [
-// 			'@swc/jest',
-// 			{
-// 				jsc: {
-// 					parser: {
-// 						syntax: 'typescript',
-// 						tsx: true
-// 					},
-// 					transform: {
-// 						react: {
-// 							runtime: 'automatic'
-// 						}
-// 					}
-// 				}
-// 			}
-// 		]
-// 	}
-// };
-
-// TODO update the jest-config-ts lib
-// delete jestTsConfig.transform;
-// delete jestTsConfig.globals;=
-
 module.exports = merge(jestConfig, {
 	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
 });

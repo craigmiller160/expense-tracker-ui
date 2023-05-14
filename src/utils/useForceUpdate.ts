ae90a9ad5c43da8023bf15df603ff04e82fe7471
Craig Miller <craigmiller160@gmail.com>
2023-05-14 12:58:56 -0400

    More cleanup

--- src/utils/useForceUpdate.ts
+++
@@ -1,7 +0,0 @@
-import { useCallback, useState } from 'react';
-
-export type ForceUpdate = () => void;
-export const useForceUpdate = (): ForceUpdate => {
-	const [, setState] = useState<number>(0);
-	return useCallback(() => setState((prev) => prev + 1), [setState]);
-};

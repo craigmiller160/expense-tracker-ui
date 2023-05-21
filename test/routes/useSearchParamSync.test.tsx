3ae0cf42d15345b0719d2320603e2c6729dada46
Craig Miller <craigmiller160@gmail.com>
2023-05-21 19:49:05 -0400

    Refactored tests

--- test/routes/useSearchParamSync.test.tsx
+++ test/routes/useSearchParamSync.test.tsx
@@ -6,7 +6,7 @@ import {
 } from '../../src/routes/useSearchParamSync';
 import { useImmer } from 'use-immer';
 import { useEffect } from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, useSearchParams } from 'react-router-dom';
 import { InitialEntry } from 'history';
 import { useLocation } from 'react-router';
 import { render, screen, waitFor } from '@testing-library/react';
@@ -29,16 +29,23 @@ const TestComponent = () => {
 		count: 0
 	});
 	const location = useLocation();
+	const [, setSearchParams] = useSearchParams();
 
 	const [params, setParams] = useSearchParamSync<State>({
 		syncFromParams,
 		syncToParams
 	});
 
-	const increment = () =>
+	const incrementState = () =>
 		setState((draft) => {
 			draft.count++;
 		});
+
+	const incrementSearch = () => {
+		const newParams = new URLSearchParams(location.search);
+		newParams.set('count', (state.count + 1).toString());
+		setSearchParams(newParams);
+	};
 
 	useEffect(() => {
 		setParams(state);
@@ -49,7 +56,8 @@ const TestComponent = () => {
 			<p>State Count: {state.count}</p>
 			<p>Params Count: {params.count}</p>
 			<p>Search: {location.search}</p>
-			<button onClick={increment}>Increment</button>
+			<button onClick={incrementState}>Increment State</button>
+			<button onClick={incrementSearch}>Increment Params</button>
 		</div>
 	);
 };
@@ -76,32 +84,42 @@ describe('useSearchParamSync', () => {
 
 	it('updates search params for state change', async () => {
 		doRender('/');
-		expect(screen.getByText(/State/)).toHaveTextContent('State Count: 0');
-		expect(screen.getByText(/Params/)).toHaveTextContent('Params Count: 0');
+		expect(screen.getByText(/State Count/)).toHaveTextContent(
+			'State Count: 0'
+		);
+		expect(screen.getByText(/Params Count/)).toHaveTextContent(
+			'Params Count: 0'
+		);
 		await waitFor(() =>
 			expect(screen.getByText(/Search/)).toHaveTextContent(
 				'Search: ?count=0'
 			)
 		);
 
-		await userEvent.click(screen.getByText('Increment'));
+		await userEvent.click(screen.getByText('Increment State'));
 		await waitFor(() =>
 			expect(screen.getByText(/Search/)).toHaveTextContent(
 				'Search: ?count=1'
 			)
 		);
-		expect(screen.getByText(/State/)).toHaveTextContent('State Count: 1');
-		expect(screen.getByText(/Params/)).toHaveTextContent('Params Count: 1');
+		expect(screen.getByText(/State Count/)).toHaveTextContent(
+			'State Count: 1'
+		);
+		expect(screen.getByText(/Params Count/)).toHaveTextContent(
+			'Params Count: 1'
+		);
 	});
 
 	it('updates state for search params', async () => {
 		doRender('/?count=1');
 		await waitFor(() =>
-			expect(screen.getByText(/State/)).toHaveTextContent(
+			expect(screen.getByText(/State Count/)).toHaveTextContent(
 				'State Count: 1'
 			)
 		);
-		expect(screen.getByText(/Params/)).toHaveTextContent('Params Count: 1');
+		expect(screen.getByText(/Params Count/)).toHaveTextContent(
+			'Params Count: 1'
+		);
 		expect(screen.getByText(/Search/)).toHaveTextContent(
 			'Search: ?count=1'
 		);

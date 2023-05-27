f430d7c23d719c3e198a2808b45ad40effff0536
Craig Miller <craigmiller160@gmail.com>
2023-05-27 16:03:58 -0400

    Stubbed out new tests

--- test/routes/useFormWithSearchParamSync.test.tsx
+++ test/routes/useFormWithSearchParamSync.test.tsx
@@ -1,6 +1,14 @@
 export {};
 describe('useFormWithSearchParamSync', () => {
-	it('tests', () => {
+	it('sets form default values in params', () => {
+		throw new Error();
+	});
+
+	it('updates form and params', async () => {
+		throw new Error();
+	});
+
+	it('merges initial params values with default form values', () => {
 		throw new Error();
 	});
 });

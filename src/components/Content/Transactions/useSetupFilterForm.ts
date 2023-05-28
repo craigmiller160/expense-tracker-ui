6e00c5bd566c2d7dcca49b55c5c67135a6454151
Craig Miller <craigmiller160@gmail.com>
2023-05-28 11:28:51 -0400

    Fixing more type errors

--- src/components/Content/Transactions/useSetupFilterForm.ts
+++ src/components/Content/Transactions/useSetupFilterForm.ts
@@ -16,24 +16,23 @@ import { UseFormReturn } from 'react-hook-form';
 import { useGetAllCategories } from '../../../ajaxapi/query/CategoryQueries';
 import { useCategoriesToCategoryOptions } from '../../../utils/categoryUtils';
 import { useFormWithSearchParamSync } from '../../../routes/useFormWithSearchParamSync';
-import { setOrDeleteParam } from '../../../routes/ParamsWrapper';
+import { ParamsWrapper } from '../../../routes/ParamsWrapper';
 import isValid from 'date-fns/isValid/index';
 import { useCallback } from 'react';
 
 const formToParams: SyncToParams<TransactionSearchForm> = (form, params) => {
-	const setOrDelete = setOrDeleteParam(params);
-	params.set('direction', form.direction);
+	params.setOrDelete('direction', form.direction);
 	const startDate =
 		form.startDate && isValid(form.startDate) ? form.startDate : undefined;
-	setOrDelete('startDate', startDate, formatServerDate);
+	params.setOrDelete('startDate', startDate, formatServerDate);
 	const endDate =
 		form.endDate && isValid(form.endDate) ? form.endDate : undefined;
-	setOrDelete('endDate', endDate, formatServerDate);
-	setOrDelete('category', form.category?.value);
-	params.set('isNotConfirmed', form.isNotConfirmed.toString());
-	params.set('isDuplicate', form.isDuplicate.toString());
-	params.set('isNotCategorized', form.isNotCategorized.toString());
-	params.set('isPossibleRefund', form.isPossibleRefund.toString());
+	params.setOrDelete('endDate', endDate, formatServerDate);
+	params.setOrDelete('category', form.category?.value);
+	params.setOrDelete('isNotConfirmed', form.isNotConfirmed.toString());
+	params.setOrDelete('isDuplicate', form.isDuplicate.toString());
+	params.setOrDelete('isNotCategorized', form.isNotCategorized.toString());
+	params.setOrDelete('isPossibleRefund', form.isPossibleRefund.toString());
 };
 
 const parseSortDirection = (value: string | null): SortDirection =>
@@ -48,50 +47,54 @@ const parseDate = (value: string | null): Date | null => {
 	return parseServerDate(value);
 };
 
-const parseCategory = (
-	categories: ReadonlyArray<CategoryOption>,
-	categoryId: string | null
-): CategoryOption | null => {
-	if (!categoryId) {
-		return null;
-	}
+const parseCategory =
+	(categories: ReadonlyArray<CategoryOption>) =>
+	(categoryId: string | null): CategoryOption | null => {
+		if (!categoryId) {
+			return null;
+		}
 
-	return categories.find((cat) => cat.value === categoryId) ?? null;
-};
+		return categories.find((cat) => cat.value === categoryId) ?? null;
+	};
 
-const parseBoolean = (value: string | null, defaultValue: boolean): boolean => {
-	if (!value) {
-		return defaultValue;
-	}
-
-	return /true/i.test(value);
-};
+const parseBoolean = (value: string): boolean => /true/i.test(value);
 
 const formFromParams =
 	(
 		categories: ReadonlyArray<CategoryOption>
 	): SyncFromParams<TransactionSearchForm> =>
 	(params) => {
-		// TODO finish this
-		const direction = parseSortDirection(params.get('direction'));
-		const startDate = parseDate(params.get('startDate'));
-		const endDate = parseDate(params.get('endDate'));
-		const category = parseCategory(categories, params.get('category'));
-		const isNotConfirmed = parseBoolean(
-			params.get('isNotConfirmed'),
-			transactionSearchFormDefaultValues.isNotConfirmed
+		const direction = params.getOrDefault(
+			'direction',
+			transactionSearchFormDefaultValues.direction,
+			parseSortDirection
 		);
-		const isDuplicate = parseBoolean(
-			params.get('isDuplicate'),
-			transactionSearchFormDefaultValues.isDuplicate
+		const startDate = params.getOrDefault('startDate', null, parseDate);
+		const endDate = params.getOrDefault('endDate', null, parseDate);
+		const category = params.getOrDefault(
+			'category',
+			null,
+			parseCategory(categories)
 		);
-		const isNotCategorized = parseBoolean(
-			params.get('isNotCategorized'),
-			transactionSearchFormDefaultValues.isNotCategorized
+		const isNotConfirmed = params.getOrDefault(
+			'isNotConfirmed',
+			transactionSearchFormDefaultValues.isNotConfirmed,
+			parseBoolean
 		);
-		const isPossibleRefund = parseBoolean(
-			params.get('isPossibleRefund'),
-			transactionSearchFormDefaultValues.isPossibleRefund
+		const isDuplicate = params.getOrDefault(
+			'isDuplicate',
+			transactionSearchFormDefaultValues.isDuplicate,
+			parseBoolean
+		);
+		const isNotCategorized = params.getOrDefault(
+			'isNotCategorized',
+			transactionSearchFormDefaultValues.isNotCategorized,
+			parseBoolean
+		);
+		const isPossibleRefund = params.getOrDefault(
+			'isPossibleRefund',
+			transactionSearchFormDefaultValues.isPossibleRefund,
+			parseBoolean
 		);
 
 		return {
@@ -110,7 +113,7 @@ export const useSetupFilterForm = (): UseFormReturn<TransactionSearchForm> => {
 	const { data } = useGetAllCategories();
 	const categories = useCategoriesToCategoryOptions(data);
 	const memoizedFormFromParams = useCallback(
-		(params: URLSearchParams) => formFromParams(categories)(params),
+		(params: ParamsWrapper) => formFromParams(categories)(params),
 		[categories]
 	);
 	return useFormWithSearchParamSync<TransactionSearchForm>({

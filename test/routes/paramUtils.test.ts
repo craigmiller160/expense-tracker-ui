9024de157c3895dd05b4c7b5506bb4e09be8d8de
Craig Miller <craigmiller160@gmail.com>
2023-05-28 10:23:40 -0400

    Stubbed out new tests

--- test/routes/paramUtils.test.ts
+++ test/routes/paramUtils.test.ts
@@ -3,51 +3,73 @@ import { identity } from 'fp-ts/es6/function';
 import * as Try from '@craigmiller160/ts-functions/es/Try';
 
 describe('paramUtils', () => {
-	it('sets param for string value', () => {
-		const params = new URLSearchParams();
-		setOrDeleteParam(params)('foo', 'bar');
-		expect(params.get('foo')).toEqual('bar');
+	describe('setOrDeleteParam', () => {
+		it('sets param for string value', () => {
+			const params = new URLSearchParams();
+			setOrDeleteParam(params)('foo', 'bar');
+			expect(params.get('foo')).toEqual('bar');
+		});
+
+		it('deletes param for null value', () => {
+			const params = new URLSearchParams();
+			params.set('foo', 'bar');
+			setOrDeleteParam(params)('foo', null);
+			expect(params.get('foo')).toBeNull();
+		});
+
+		it('deletes param for undefined value', () => {
+			const params = new URLSearchParams();
+			params.set('foo', 'bar');
+			setOrDeleteParam(params)('foo', undefined);
+			expect(params.get('foo')).toBeNull();
+		});
+
+		it('sets param for falsy, non-nullish value', () => {
+			const params = new URLSearchParams();
+			setOrDeleteParam(params)('foo', '');
+			expect(params.get('foo')).toEqual('');
+		});
+
+		it('transforms non-string param', () => {
+			const params = new URLSearchParams();
+			setOrDeleteParam(params)('foo', 1, (i) => i.toString());
+			expect(params.get('foo')).toEqual('1');
+		});
+
+		it('transforms string param', () => {
+			const params = new URLSearchParams();
+			setOrDeleteParam(params)('foo', 'bar', identity);
+			expect(params.get('foo')).toEqual('bar');
+		});
+
+		it('throws exception for non-string value without transform', () => {
+			const params = new URLSearchParams();
+			const result = Try.tryCatch(() =>
+				setOrDeleteParam(params)('foo', 1)
+			);
+			expect(result).toEqualLeft(
+				new Error(
+					'Must provide transform to set non-string value on params'
+				)
+			);
+		});
 	});
 
-	it('deletes param for null value', () => {
-		const params = new URLSearchParams();
-		params.set('foo', 'bar');
-		setOrDeleteParam(params)('foo', null);
-		expect(params.get('foo')).toBeNull();
-	});
+	describe('getOrDefaultParam', () => {
+		it('gets param value', () => {
+			throw new Error();
+		});
 
-	it('deletes param for undefined value', () => {
-		const params = new URLSearchParams();
-		params.set('foo', 'bar');
-		setOrDeleteParam(params)('foo', undefined);
-		expect(params.get('foo')).toBeNull();
-	});
+		it('gets default value for non-existent param', () => {
+			throw new Error();
+		});
 
-	it('sets param for falsy, non-nullish value', () => {
-		const params = new URLSearchParams();
-		setOrDeleteParam(params)('foo', '');
-		expect(params.get('foo')).toEqual('');
-	});
+		it('gets and transforms param value', () => {
+			throw new Error();
+		});
 
-	it('transforms non-string param', () => {
-		const params = new URLSearchParams();
-		setOrDeleteParam(params)('foo', 1, (i) => i.toString());
-		expect(params.get('foo')).toEqual('1');
-	});
-
-	it('transforms string param', () => {
-		const params = new URLSearchParams();
-		setOrDeleteParam(params)('foo', 'bar', identity);
-		expect(params.get('foo')).toEqual('bar');
-	});
-
-	it('throws exception for non-string value without transform', () => {
-		const params = new URLSearchParams();
-		const result = Try.tryCatch(() => setOrDeleteParam(params)('foo', 1));
-		expect(result).toEqualLeft(
-			new Error(
-				'Must provide transform to set non-string value on params'
-			)
-		);
+		it('gets default non-string value for non-existent param', () => {
+			throw new Error();
+		});
 	});
 });

b7f2668063e1b3540399e1b534c01fe20eb30062
Craig Miller <craigmiller160@gmail.com>
2023-06-11 11:02:32 -0400

    Fixing some tests

--- test/ajaxapi/service/TransactionService.test.ts
+++ test/ajaxapi/service/TransactionService.test.ts
@@ -5,46 +5,53 @@ import * as Time from '@craigmiller160/ts-functions/es/Time';
 describe('TransactionService', () => {
 	describe('requestToQuery', () => {
 		it('only required fields', () => {
-			// const result = requestToQuery({
-			// 	pageNumber: 1,
-			// 	pageSize: 10,
-			// 	sortDirection: SortDirection.ASC,
-			// 	sortKey: TransactionSortKey.EXPENSE_DATE,
-			// 	startDate: null,
-			// 	endDate: null
-			// });
-			// expect(result).toEqual(
-			// 	'pageNumber=1&pageSize=10&sortDirection=ASC&sortKey=EXPENSE_DATE'
-			// );
-			throw new Error();
+			const result = requestToQuery({
+				pageNumber: 1,
+				pageSize: 10,
+				sortDirection: SortDirection.ASC,
+				sortKey: TransactionSortKey.EXPENSE_DATE,
+				startDate: null,
+				endDate: null,
+				categorized: 'ALL',
+				confirmed: 'ALL',
+				duplicate: 'ALL',
+				possibleRefund: 'ALL'
+			});
+			expect(result).toEqual(
+				'pageNumber=1&pageSize=10&sortDirection=ASC&sortKey=EXPENSE_DATE&categorized=ALL&confirmed=ALL&duplicate=ALL&possibleRefund=ALL'
+			);
 		});
 
 		it('all fields', () => {
-			// const startDate = Time.set({
-			// 	year: 2022,
-			// 	month: 0,
-			// 	date: 1
-			// })(new Date());
-			// const endDate = Time.set({
-			// 	year: 2022,
-			// 	month: 0,
-			// 	date: 2
-			// })(new Date());
-			// const result = requestToQuery({
-			// 	pageNumber: 1,
-			// 	pageSize: 10,
-			// 	sortDirection: SortDirection.ASC,
-			// 	sortKey: TransactionSortKey.EXPENSE_DATE,
-			// 	startDate,
-			// 	endDate,
-			// 	isConfirmed: true,
-			// 	isCategorized: true,
-			// 	isDuplicate: false,
-			// 	categoryIds: ['1', '2']
-			// });
-			// expect(result).toEqual(
-			// 	'pageNumber=1&pageSize=10&sortDirection=ASC&sortKey=EXPENSE_DATE&startDate=2022-01-01&endDate=2022-01-02&isConfirmed=true&isCategorized=true&isDuplicate=false&categoryIds=1%2C2'
-			// );
+			const startDate = Time.set({
+				year: 2022,
+				month: 0,
+				date: 1
+			})(new Date());
+			const endDate = Time.set({
+				year: 2022,
+				month: 0,
+				date: 2
+			})(new Date());
+			const result = requestToQuery({
+				pageNumber: 1,
+				pageSize: 10,
+				sortDirection: SortDirection.ASC,
+				sortKey: TransactionSortKey.EXPENSE_DATE,
+				startDate,
+				endDate,
+				confirmed: 'ALL',
+				categorized: 'ALL',
+				duplicate: 'ALL',
+				possibleRefund: 'ALL',
+				categoryIds: ['1', '2']
+			});
+			expect(result).toEqual(
+				'pageNumber=1&pageSize=10&sortDirection=ASC&sortKey=EXPENSE_DATE&startDate=2022-01-01&endDate=2022-01-02&confirmed=ALL&categorized=ALL&duplicate=ALL&possibleRefund=ALL&categoryIds=1%2C2'
+			);
+		});
+
+		it('removes category ids if categorized is NO', () => {
 			throw new Error();
 		});
 	});

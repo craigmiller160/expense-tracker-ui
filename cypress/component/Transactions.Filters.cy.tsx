f448ccca09f114083220ccea7513adbb7cca43bd
Craig Miller <craigmiller160@gmail.com>
2023-06-11 11:13:00 -0400

    Stubbed out new cypress tests

---
+++ cypress/component/Transactions.Filters.cy.tsx
@@ -0,0 +1,21 @@
+describe('Transactions Filters', () => {
+	it('possible refund control', () => {
+		throw new Error();
+	});
+
+	it('category control', () => {
+		throw new Error();
+	});
+
+	it('duplicate control', () => {
+		throw new Error();
+	});
+
+	it('categorized control', () => {
+		throw new Error();
+	});
+
+	it('confirmed control', () => {
+		throw new Error();
+	});
+});

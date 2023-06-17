bdbb33e7784912d1544ee4c46e15d754ced2c531
Craig Miller <craigmiller160@gmail.com>
2023-06-17 14:18:31 -0400

    Stubbed out tests

---
+++ test/components/Content/Reports/utils.test.ts
@@ -0,0 +1,11 @@
+import { getMonthLink } from '../../../../src/components/Content/Reports/utils';
+
+describe('Reports utils', () => {
+	it('getMonthLink', () => {
+		throw new Error();
+	});
+
+	it('getMonthAndCategoryLink', () => {
+		throw new Error();
+	});
+});

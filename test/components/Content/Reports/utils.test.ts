bad5ca86677c454c82d1bc1433a09174416c9089
Craig Miller <craigmiller160@gmail.com>
2023-06-17 14:19:52 -0400

    Implemented tests

--- test/components/Content/Reports/utils.test.ts
+++ test/components/Content/Reports/utils.test.ts
@@ -1,11 +1,23 @@
-import { getMonthLink } from '../../../../src/components/Content/Reports/utils';
+import {
+	getMonthAndCategoryLink,
+	getMonthLink
+} from '../../../../src/components/Content/Reports/utils';
 
 describe('Reports utils', () => {
 	it('getMonthLink', () => {
-		throw new Error();
+		const link = getMonthLink('2022-01-12');
+		expect(link).toEqual(
+			'/expense-tracker/transactions?startDate=2022-01-01&endDate=2022-01-31'
+		);
 	});
 
 	it('getMonthAndCategoryLink', () => {
-		throw new Error();
+		const link = getMonthAndCategoryLink(
+			'2022-01-12',
+			'c0792ddd-381d-4724-a4b2-790929aff827'
+		);
+		expect(link).toEqual(
+			'/expense-tracker/transactions?startDate=2022-01-01&endDate=2022-01-31&category=c0792ddd-381d-4724-a4b2-790929aff827'
+		);
 	});
 });

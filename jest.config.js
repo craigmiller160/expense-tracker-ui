68daaa626d89fabf93ae3b16fc1889fc4437fa0a
Craig Miller <craigmiller160@gmail.com>
2023-09-02 15:17:09 -0400

    Working on lib and esmodule issues

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ const jestConfig = require('@craigmiller160/jest-config');
 const path = require('path');
 
 module.exports = merge(jestConfig, {
-	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')]
+	setupFilesAfterEnv: [path.join(process.cwd(), 'test', 'setup.tsx')],
+	moduleNameMapper: {
+		...jestConfig.moduleNameMapper,
+		'^nanoid$': import('nanoid')
+	}
 });

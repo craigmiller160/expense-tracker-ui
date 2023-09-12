06ed271b3b8725a94c3f6b42a35953b476de0a2c
Craig Miller <craigmiller160@gmail.com>
2023-09-12 10:42:33 -0400

    Fixing types

--- cypress/component/testutils/constants/transactions.ts
+++ cypress/component/testutils/constants/transactions.ts
@@ -1,9 +1,25 @@
 import allTransactions from '../../../fixtures/allTransactions.json';
 import transactionDetails from '../../../fixtures/transactionDetails.json';
-import possibleDuplicates from '../../../fixtures/possibleDuplicates.json';
+import possibleDuplicatesJson from '../../../fixtures/possibleDuplicates.json';
 import { SelectOption } from '@craigmiller160/react-hook-form-material-ui';
 
-export { allTransactions, transactionDetails, possibleDuplicates };
+type PossibleDuplicates = Readonly<{
+	transactions: ReadonlyArray<
+		Readonly<{
+			id: string;
+			created: string;
+			updated: string;
+			categoryName: string;
+			confirmed: boolean;
+		}>
+	>;
+	pageNumber: number;
+	totalItems: number;
+}>;
+
+export const possibleDuplicates = possibleDuplicatesJson as PossibleDuplicates;
+
+export { allTransactions, transactionDetails };
 
 export const orderByOptions: ReadonlyArray<SelectOption<string>> = [
 	{ value: 'ASC', label: 'Newest to Oldest' },

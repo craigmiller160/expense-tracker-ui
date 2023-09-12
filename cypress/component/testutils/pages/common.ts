1c2278bf6453a5d42522a9b666e6454bba3d40a9
Craig Miller <craigmiller160@gmail.com>
2023-09-12 11:28:44 -0400

    Fixed type errors

--- cypress/component/testutils/pages/common.ts
+++ cypress/component/testutils/pages/common.ts
@@ -9,7 +9,8 @@ const getMultipleSelectValues = (
 const getOpenSelectOptions = (): Chainable<JQuery> =>
 	cy.get('.MuiList-root li[role="option"]');
 
-const dismissPopupOptions = (): Chainable<JQuery<HTMLBodyElement>> => cy.get('body').click(0, 0);
+const dismissPopupOptions = (): Chainable<JQuery<HTMLBodyElement>> =>
+	cy.get('body').click(0, 0);
 
 export const commonPage = {
 	getOpenAutoCompleteOptions,

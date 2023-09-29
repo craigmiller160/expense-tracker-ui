f4eb95bb136957c5978f478eef6bf3f581110d77
Craig Miller <craigmiller160@gmail.com>
2023-09-29 18:28:34 -0400

    Fixing file

--- test/testutils/renderApp.tsx
+++ test/testutils/renderApp.tsx
@@ -1,8 +1,6 @@
+import { vi } from 'vitest';
 import { render, RenderResult } from '@testing-library/react';
 import { App } from '../../src/components/App';
-import { Sleep } from '@craigmiller160/ts-functions';
-import * as Task from 'fp-ts/Task';
-import { pipe } from 'fp-ts/function';
 import { BrowserRouter } from 'react-router-dom';
 import {
 	KeycloakAuth,
@@ -21,18 +19,14 @@ const keycloakAuth: KeycloakAuth = {
 	logout: vi.fn()
 };
 
-export const renderApp = (config?: RenderConfig): Promise<RenderResult> => {
+export const renderApp = (config?: RenderConfig): RenderResult => {
 	window.history.replaceState({}, '', config?.initialPath ?? '/');
 	const queryClient = newQueryClient();
-	const result = render(
+	return render(
 		<BrowserRouter basename="/">
 			<KeycloakAuthContext.Provider value={keycloakAuth}>
 				<App queryClient={queryClient} />
 			</KeycloakAuthContext.Provider>
 		</BrowserRouter>
 	);
-	return pipe(
-		Sleep.sleep(100),
-		Task.map(() => result)
-	)();
 };

78abf2fe0c9501cd13be5024218da4d1b648ea4d
Craig Miller <craigmiller160@gmail.com>
2023-12-06 22:35:17 -0500

    Making progress

--- src/utils/debounceAsync.ts
+++ src/utils/debounceAsync.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument */
 
 type AsyncFunction<T> = (...args: any[]) => Promise<T>;
 

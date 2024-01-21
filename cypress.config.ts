a7c3914b78dfd1dda64acc2f2cf09905dc445613
Craig Miller <craigmiller160@gmail.com>
2024-01-21 11:41:27 -0500

    Restored screenshots

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-	screenshotOnRunFailure: false,
+	screenshotOnRunFailure: true,
 	video: false,
 	component: {
 		devServer: {

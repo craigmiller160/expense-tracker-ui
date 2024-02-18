92e0f66521610aca30acd04b8ec5714f486c5498
Craig Miller <craigmiller160@gmail.com>
2024-02-18 14:09:52 -0500

    More request tracking

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import './request-tracker';
+import './request-tracker2';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')

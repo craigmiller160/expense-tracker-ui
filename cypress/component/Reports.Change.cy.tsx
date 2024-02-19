b73d41ad8fd06c0b7711b259cec6cda418b08067
Craig Miller <craigmiller160@gmail.com>
2024-02-18 19:08:01 -0500

    Progress

--- cypress/component/Reports.Change.cy.tsx
+++ cypress/component/Reports.Change.cy.tsx
@@ -1,5 +1,24 @@
+import { categoriesApi } from './testutils/apis/categories';
+import { reportsApi } from './testutils/apis/reports';
+import { needsAttentionApi } from './testutils/apis/needsAttention';
+import { mountApp } from './testutils/mountApp';
+import { reportsPage } from './testutils/pages/reports';
+import { reports } from './testutils/constants/reports';
+
 describe('Reports.Change', () => {
 	it('shows change between months when previous month has category', () => {
+		categoriesApi.getUnknownCategory();
+		reportsApi.getDefaultSpendingByMonthAndCategory();
+		categoriesApi.getAllCategories();
+		needsAttentionApi.getNeedsAttention_none();
+		mountApp({
+			initialRoute: '/reports'
+		});
+		reportsPage.getTitle().contains('Reports');
+		reportsPage.getTableTitle().contains('Spending by Month & Category');
+		reportsPage
+			.getRootTableRows()
+			.should('have.length', reports.reports.length);
 		throw new Error();
 	});
 

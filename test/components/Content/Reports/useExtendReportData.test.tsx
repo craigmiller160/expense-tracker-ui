53425112a4420d427e3740fe1d8d85f4e9fdf903
Craig Miller <craigmiller160@gmail.com>
2024-02-20 18:33:07 -0500

    Working on test data

--- test/components/Content/Reports/useExtendReportData.test.tsx
+++ test/components/Content/Reports/useExtendReportData.test.tsx
@@ -1,3 +1,7 @@
 import { test } from 'vitest';
+import type { ReportPageResponse } from '../../../../src/types/generated/expense-tracker';
+import reportResponseForExtension from '../../../__data__/report_response_for_extension.json';
+
+const data: ReportPageResponse = reportResponseForExtension;
 
 test.fails('extends report data');
